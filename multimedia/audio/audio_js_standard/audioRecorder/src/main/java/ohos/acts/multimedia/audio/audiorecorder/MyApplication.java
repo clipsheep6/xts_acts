5189e4c526b26b8bffbefda76ac1a697b5d5fa70
tianfenxia <tianfenxia1@huawei.com>
2021-10-08 11:27:53 +0800

    tianfenxia1@huawei.com
    Signed-off-by: tianfenxia <tianfenxia1@huawei.com>

---
+++ multimedia/audio/audio_js_standard/audioRecorder/src/main/java/ohos/acts/multimedia/audio/audiorecorder/MyApplication.java
@@ -0,0 +1,10 @@
+package ohos.acts.multimedia.audio.audiorecorder;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
