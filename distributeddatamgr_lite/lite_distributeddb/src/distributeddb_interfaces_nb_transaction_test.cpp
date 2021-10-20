94be69cfdf3b2c4cd856612ae460d74d8b913929
lidw <lidongwei6@huawei.com>
2021-10-20 17:24:30 +0800

    tmp
    Change-Id: I743d68b35862b48b7ec31e0cf44af0087ef01aa5

--- distributeddatamgr_lite/lite_distributeddb/src/distributeddb_interfaces_nb_transaction_test.cpp
+++ distributeddatamgr_lite/lite_distributeddb/src/distributeddb_interfaces_nb_transaction_test.cpp
@@ -15,21 +15,39 @@
 
 #include <gtest/gtest.h>
 
-#include "distributeddb_data_generate_unit_test.h"
-#include "distributeddb_tools_unit_test.h"
-#include "db_errno.h"
-#include "log_print.h"
-#include "db_common.h"
+// #include "distributeddb_data_generate_unit_test.h"
+// #include "distributeddb_tools_unit_test.h"
+// #include "db_errno.h"
+// #include "log_print.h"
+// #include "db_common.h"
+#include "kv_store_nb_delegate.h"
+#include "kv_store_delegate_manager.h"
 
 using namespace testing::ext;
 using namespace DistributedDB;
-using namespace DistributedDBUnitTest;
+// using namespace DistributedDBUnitTest;
 using namespace std;
 
 namespace {
+// const std::string CREATE_IF_NECESSARY = "createIfNecessary";
+// const std::string DATABASE_TYPE = "databaseType";
+// const std::string DATA_DIR = "dataDir";
+const std::string USER_ID = "userId";
+const std::string APP_ID = "appId";
+const std::string STORE_ID = "storeId";
+// const std::string FILE_NAME = "fileName";
+// const std::string MEMORY_MODE = "memoryMode";
+// const std::string ENCRYPTED_MODE = "isEncryptedDb";
+// const std::string IDENTIFIER_DATA = "identifier";
+// const std::string IDENTIFIER_DIR = "identifierDir";
+// const std::string FIRST_OPEN_IS_READ_ONLY = "firstOpenIsReadOnly";
+// const std::string CREATE_DIR_BY_STORE_ID_ONLY = "createDirByStoreIdOnly";
+// const std::string SECURITY_LABEL = "securityLabel";
+// const std::string SECURITY_FLAG = "securityFlag";
+// const std::string CONFLICT_RESOLVE_POLICY = "conflictResolvePolicy";
 // define some variables to init a KvStoreDelegateManager object.
 KvStoreDelegateManager g_mgr(APP_ID, USER_ID);
-string g_testDir;
+string g_testDir{"/nfs"};
 KvStoreConfig g_config;
 
 // define the g_kvNbDelegateCallback, used to get some information when open a kv store.
@@ -58,7 +76,7 @@ public:
 
 void DistributedDBInterfacesNBTransactionTest::SetUpTestCase(void)
 {
-    DistributedDBToolsUnitTest::TestDirInit(g_testDir);
+    // DistributedDBToolsUnitTest::TestDirInit(g_testDir);
     g_config.dataDir = g_testDir;
     g_mgr.SetKvStoreConfig(g_config);
     g_mgr.SetProcessLabel("DistributedDBInterfacesNBTransactionTest", "test");
@@ -66,18 +84,18 @@ void DistributedDBInterfacesNBTransactionTest::SetUpTestCase(void)
 
 void DistributedDBInterfacesNBTransactionTest::TearDownTestCase(void)
 {
-    if (DistributedDBToolsUnitTest::RemoveTestDbFiles(g_testDir) != 0) {
-        LOGE("rm test db files error!");
-    }
+    // if (DistributedDBToolsUnitTest::RemoveTestDbFiles(g_testDir) != 0) {
+    //     LOGE("rm test db files error!");
+    // }
 }
 
-void DistributedDBInterfacesNBTransactionTest::SetUp(void)
+void DistributedDBInterfacesNBTransactionTest::SetUp()
 {
     g_kvDelegateStatus = INVALID_ARGS;
     g_kvNbDelegatePtr = nullptr;
 }
 
-void DistributedDBInterfacesNBTransactionTest::TearDown(void)
+void DistributedDBInterfacesNBTransactionTest::TearDown()
 {}
 
 /**
@@ -89,7 +107,10 @@ void DistributedDBInterfacesNBTransactionTest::TearDown(void)
  */
 HWTEST_F(DistributedDBInterfacesNBTransactionTest, start001, Function | SmallTest | Level0)
 {
-    const KvStoreNbDelegate::Option option = {true, false};
+    KvStoreNbDelegate::Option option;  // {
+    option.createIfNecessary = true;
+    option.isMemoryDb = false;
+    // };
     g_mgr.GetKvStore("distributed_nb_transaction_start001", option, g_kvNbDelegateCallback);
     ASSERT_TRUE(g_kvNbDelegatePtr != nullptr);
     EXPECT_TRUE(g_kvDelegateStatus == OK);
