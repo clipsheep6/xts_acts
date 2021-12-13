1ede7fcef7cc8f867c72275040efb12193d8535a
zhangxingxia <zhangxingxia050@chinasoftinc.com>
2021-12-13 14:11:46 +0800

    环境目录测试用例代码提交
    Signed-off-by: zhangxingxia <zhangxingxia050@chinasoftinc.com>

---
+++ storage/storageenvironmentjstest/src/main/js/default/test/environment.test.js
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  isIntNum,
+  describe,
+  it,
+  expect,
+  environment,
+  isString,
+} from "./Common";
+
+describe("environment", function () {
+  /**
+   * @tc.number SUB_DF_ENVIRONMENT_GET_STORAGE_DATA_0000
+   * @tc.name environment_test_get_storage_data_async_000
+   * @tc.desc Test getStorageData() interfaces.
+   * @tc.size MEDIUM
+   * @tc.type Function
+   * @tc.level Level 0
+   * @tc.require
+   */
+  it("environment_test_get_storage_data_async_000", 0, async function (done) {
+    try {
+      environment.getStorageData((err, url) => {
+        expect(isString(url)).assertTrue();
+        console.log("environment_test_get_storage_data_async_000:" + url);
+        done();
+      });
+    } catch (e) {
+      console.log("environment_test_get_storage_data_async_000 has failed for " + e);
+      expect(null).assertFail();
+    }
+  });
+
+  /**
+   * @tc.number SUB_DF_ENVIRONMENT_GET_STORAGE_DATA_0010
+   * @tc.name environment_test_get_storage_data_async_001
+   * @tc.desc Test getStorageData() interfaces.
+   * @tc.size MEDIUM
+   * @tc.type Function
+   * @tc.level Level 0
+   * @tc.require
+   */
+  it("environment_test_get_storage_data_async_001", 0, async function (done) {
+    try {
+      await environment.getStorageData("123");
+      expect(null).assertFail();
+    } catch (e) {
+      console.log("environment_test_get_storage_data_async_001 has failed for " + e);
+      expect(!!e).assertTrue();
+    }
+    done();
+  });
+
+  /**
+   * @tc.number SUB_DF_ENVIRONMENT_GET_STORAGE_DATA_0020
+   * @tc.name environment_test_get_storage_data_async_002
+   * @tc.desc Test getStorageData() interfaces.
+   * @tc.size MEDIUM
+   * @tc.type Function
+   * @tc.level Level 0
+   * @tc.require
+   */
+  it("environment_test_get_storage_data_async_002", 0, async function (done) {
+    try {
+      let url = await environment.getStorageData();
+      expect(isString(url)).assertTrue();
+      console.log("environment_test_get_storage_data_async_002:" + url);
+      done();
+    } catch (e) {
+      console.log("environment_test_get_storage_data_async_002 has failed for " + e);
+      expect(null).assertFail();
+    }
+  });
+
+  /**
+   * @tc.number SUB_DF_ENVIRONMENT_GET_USER_DATA_0000
+   * @tc.name environment_test_get_user_data_async_000
+   * @tc.desc Test getUserData() interfaces.
+   * @tc.size MEDIUM
+   * @tc.type Function
+   * @tc.level Level 0
+   * @tc.require
+   */
+  it("environment_test_get_user_data_async_000", 0, async function (done) {
+    try {
+      let url = await environment.getUserData();
+      expect(isString(url)).assertTrue();
+      console.log("environment_test_get_user_data_async_000:" + url);
+      done();
+    } catch (e) {
+      console.log("environment_test_get_user_data_async_000 has failed for " + e);
+      expect(null).assertFail();
+    }
+  });
+
+  /**
+   * @tc.number SUB_DF_ENVIRONMENT_GET_USER_DATA_0010
+   * @tc.name environment_test_get_user_data_async_001
+   * @tc.desc Test getUserData() interfaces.
+   * @tc.size MEDIUM
+   * @tc.type Function
+   * @tc.level Level 0
+   * @tc.require
+   */
+  it("environment_test_get_user_data_async_001", 0, async function (done) {
+    try {
+      environment.getUserData((err, url) => {
+        expect(isString(url)).assertTrue();
+        console.log("environment_test_get_user_data_async_001:" + url);
+        done();
+      });
+    } catch (e) {
+      console.log("environment_test_get_user_data_async_001 has failed for " + e);
+      expect(null).assertFail();
+    }
+  });
+
+  /**
+   * @tc.number SUB_DF_ENVIRONMENT_GET_USER_DATA_0020
+   * @tc.name environment_test_get_user_data_async_002
+   * @tc.desc Test getUserData() interfaces.
+   * @tc.size MEDIUM
+   * @tc.type Function
+   * @tc.level Level 0
+   * @tc.require
+   */
+  it("environment_test_get_user_data_async_002", 0, async function (done) {
+    try {
+      await environment.getUserData("123");
+      expect(null).assertFail();
+    } catch (e) {
+      console.log("environment_test_get_user_data_async_002 has failed for " + e);
+      expect(!!e).assertTrue();
+    }
+    done();
+  });
+});
