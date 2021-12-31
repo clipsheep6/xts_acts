fcff690ab16e01424ca458787c58af7a4417d8bd
zhang-xiaobo1997 <tony.zhangxiaobo@huawei.com>
2021-12-31 16:59:22 +0800

    modify for code style
    Signed-off-by: zhang-xiaobo1997 <tony.zhangxiaobo@huawei.com>

--- multimedia/image/image_js_standard/src/main/js/default/app.js
+++ multimedia/image/image_js_standard/src/main/js/default/app.js
@@ -1,6 +1,4 @@
 /*
- * @file app.js
- *
  * Copyright (C) 2021 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +11,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/**
+ * @file: app.js
  */
 
 export default {
