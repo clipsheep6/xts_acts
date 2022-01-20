94886f6739f9fc4bfbdb66c292894dc3fb79c1ca
yu.xu <yu.xu@neusoft.com>
2022-01-20 16:29:36 +0800

    Add thermal xts test
    Signed-off-by: Du Jiang <dujiang@neusoft.com>

---
+++ powermgr/powermgrthermal/src/main/resources/base/temp.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo 42100 > /data/sensor/battery/temp
+sleep 10
+echo 48000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
+echo 46100 > /data/sensor/battery/temp
+sleep 10
+echo 48100 > /data/sensor/battery/temp
+sleep 10
+echo 40100 > /data/sensor/battery/temp
+sleep 10
+echo -21000 > /data/sensor/battery/temp
+sleep 10
