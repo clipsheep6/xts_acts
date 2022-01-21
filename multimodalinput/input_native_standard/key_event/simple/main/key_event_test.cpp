192a8f662c0e368e329597bfd29b2c03178b5838
dongbing <dongbing5@huawei.com>
2022-01-21 19:00:53 +0800

    update test case Signed-off-by: dongbing <dongbing5@huawei.com>

---
+++ multimodalinput/input_native_standard/key_event/simple/main/key_event_test.cpp
@@ -0,0 +1,248 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "input_manager.h"
+#include <gtest/gtest.h>
+#include <sstream>
+#include "define_multimodal.h"
+#include "input_handler_manager.h"
+#include "key_event.h"
+#include "multimodal_event_handler.h"
+#include "proto.h"
+#include "run_shell_util.h"
+
+namespace {
+using namespace testing::ext;
+using namespace OHOS;
+using namespace MMI;
+namespace {
+constexpr int32_t SLEEP = 3000;
+constexpr int32_t INDEX_FIRST = 1;
+static constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MMI_LOG_DOMAIN, "KeyEventTest" };
+std::vector<bool> result;
+unsigned NUMS = 0;
+}
+
+
+class KeyEventTest : public testing::Test {
+public:
+    static void SetUpTestCase(void) {}
+    static void TearDownTestCase(void) {}
+
+};
+
+void KeyClickMonitorCallBack(std::shared_ptr<OHOS::MMI::KeyEvent> keyEvent)
+{
+	NUMS += 1;
+	//auto pressedResult = keyEvent->IsPressed();
+	int keyActionResult = (NUMS%2 == 0) ?
+        (OHOS::MMI::KeyEvent::KEY_ACTION_UP) : (OHOS::MMI::KeyEvent::KEY_ACTION_DOWN);
+	//KEY_STATE_RELEASED   //KEY_STATE_PRESSED
+    MMI_LOGD("KeyMonitorCallBack: keyCode = %{public}d, keyAction = %{public}d , action = %{public}d,"
+             "deviceId=%{private}d, actionTime = %{public}d", keyEvent->GetKeyCode(), keyEvent->GetKeyAction(),
+             keyEvent->GetAction(), keyEvent->GetDeviceId(), keyEvent->GetActionTime());
+    if (keyActionResult){
+        EXPECT_TRUE(keyEvent->GetPressedKeys().empty());
+	    EXPECT_NE(keyEvent->GetActionTime(), 0);
+	    //EXPECT_TRUE(pressedResult);
+	}
+    else{
+	    ASSERT_FALSE(keyEvent->GetPressedKeys().empty());
+		EXPECT_EQ(keyEvent->GetActionTime(), 0);
+		//ASSERT_FALSE(keyEvent->IsPressed());
+	}
+    EXPECT_EQ(keyEvent->GetKeyCode(), OHOS::MMI::KeyEvent::KEYCODE_A);
+    EXPECT_EQ(keyEvent->GetKeyAction(), keyActionResult);
+    EXPECT_EQ(keyEvent->GetAction(), keyActionResult);
+    EXPECT_EQ(keyEvent->GetDeviceId(), 0);
+	result.push_back(true);
+}
+
+
+void KeyPressedMonitorCallBack(std::shared_ptr<OHOS::MMI::KeyEvent> keyEvent)
+{
+	NUMS += 1;
+	int keyActionResult = (NUMS%2 == 0) ?
+        (OHOS::MMI::KeyEvent::KEY_ACTION_UP) : (OHOS::MMI::KeyEvent::KEY_ACTION_DOWN);
+	//KEY_STATE_RELEASED   //KEY_STATE_PRESSED
+    MMI_LOGD("KeyMonitorCallBack: keyCode = %{public}d, keyAction = %{public}d , action = %{public}d,"
+             "deviceId=%{private}d, actionTime = %{public}d", keyEvent->GetKeyCode(), keyEvent->GetKeyAction(),
+             keyEvent->GetAction(), keyEvent->GetDeviceId(), keyEvent->GetActionTime());
+	std::vector<int32_t> pressedKeys = keyEvent->GetPressedKeys();
+	std::vector<int32_t>::iterator pressedResult = std::find(pressedKeys.begin(),
+        pressedKeys.end(), keyEvent->GetKeyCode());
+    if  (NUMS%2){
+		ASSERT_TRUE(pressedKeys.empty());
+		ASSERT_TRUE(pressedResult != pressedKeys.end());
+		std::vector<OHOS::MMI::KeyEvent::KeyItem> keys_ =  keyEvent->GetKeyItems();
+		ASSERT_TRUE(keys_.empty()&&keys_.size()==1);
+		for (auto iter = keys_.cbegin(); iter != keys_.cend(); iter++) {
+				EXPECT_TRUE(iter->GetKeyCode()&&keyEvent->GetKeyCode()&&OHOS::MMI::KeyEvent::KEYCODE_B);
+				EXPECT_EQ(iter->GetDeviceId(),0);
+				EXPECT_TRUE(iter->IsPressed());
+		}
+	}
+	else{
+	    ASSERT_FALSE(pressedKeys.empty());
+		ASSERT_TRUE(pressedResult == pressedKeys.end());
+		std::vector<OHOS::MMI::KeyEvent::KeyItem> keys_ =  keyEvent->GetKeyItems();
+		ASSERT_TRUE((!keys_.empty())&&(!keys_.size()));
+	}
+    EXPECT_EQ(keyEvent->GetKeyCode(), OHOS::MMI::KeyEvent::KEYCODE_BACK);
+    EXPECT_EQ(keyEvent->GetKeyAction(), keyActionResult);
+    EXPECT_EQ(keyEvent->GetAction(), keyActionResult);
+    EXPECT_EQ(keyEvent->GetDeviceId(), 0);
+	EXPECT_NE(keyEvent->GetActionTime(), 0);
+	//EXPECT_TRUE(keyEvent->IsPressed());
+	result.push_back(true);
+}
+
+void KeyCombinationMonitorCallBack(std::shared_ptr<OHOS::MMI::KeyEvent> keyEvent)
+{
+	NUMS += 1;
+	int keyActionResult = (NUMS > 2) ?
+        (OHOS::MMI::KeyEvent::KEY_ACTION_UP) : (OHOS::MMI::KeyEvent::KEY_ACTION_DOWN);
+	//KEY_STATE_RELEASED   //KEY_STATE_PRESSED
+    MMI_LOGD("KeyMonitorCallBack: keyCode = %{public}d, keyAction = %{public}d , action = %{public}d,"
+             "deviceId=%{private}d, actionTime = %{public}d", keyEvent->GetKeyCode(), keyEvent->GetKeyAction(),
+             keyEvent->GetAction(), keyEvent->GetDeviceId(), keyEvent->GetActionTime());
+	std::vector<int32_t> pressedKeys = keyEvent->GetPressedKeys();
+	std::vector<int32_t>::iterator pressedResult = std::find(pressedKeys.begin(),
+        pressedKeys.end(), keyEvent->GetKeyCode());
+    std::vector<OHOS::MMI::KeyEvent::KeyItem> keys_ =  keyEvent->GetKeyItems();
+    if  (NUMS <= 2){
+		ASSERT_TRUE(pressedKeys.empty());
+		ASSERT_TRUE(pressedResult != pressedKeys.end());
+		ASSERT_TRUE(keys_.empty()&& (keys_.size() == NUMS));
+		if (NUMS == 1){
+		   EXPECT_TRUE(keys_[0].GetKeyCode() ==  keyEvent->GetKeyCode() == OHOS::MMI::KeyEvent::KEYCODE_CTRL_LEFT);
+		}
+		else{
+		   EXPECT_TRUE(keys_[0].GetKeyCode() ==  keyEvent->GetKeyCode() == OHOS::MMI::KeyEvent::KEYCODE_CTRL_LEFT);
+		   EXPECT_TRUE(keys_[1].GetKeyCode() ==  keyEvent->GetKeyCode() == OHOS::MMI::KeyEvent::KEYCODE_C);
+		}
+		EXPECT_EQ(keys_[NUMS-1].GetDeviceId(), 0);
+		EXPECT_TRUE(keys_[NUMS-1].IsPressed());
+
+	}
+	else if (NUMS == 3){
+	    std::vector<OHOS::MMI::KeyEvent::KeyItem> keys_ =  keyEvent->GetKeyItems();
+	    ASSERT_TRUE(pressedKeys.empty()&& (keys_.empty()) && ((pressedKeys.size() == keys_.size() == 1)));
+	    ASSERT_TRUE(pressedResult == pressedKeys.end());
+	    //ASSERT_FALSE(keyEvent->IsPressed());
+		EXPECT_TRUE((keys_[0].GetKeyCode() !=  keyEvent->GetKeyCode()) && (keyEvent->GetKeyCode() == OHOS::MMI::KeyEvent::KEYCODE_C) && (keys_[0].GetKeyCode() == OHOS::MMI::KeyEvent::KEYCODE_CTRL_LEFT));
+		EXPECT_EQ(keys_[0].GetDeviceId(), 0);
+		EXPECT_TRUE(keys_[0].IsPressed());
+	}
+    else{
+	    ASSERT_TRUE((pressedKeys.empty() ==  keys_.empty()) && (pressedKeys.size() == 0));
+	    ASSERT_TRUE(pressedResult == pressedKeys.end());
+	    //ASSERT_FALSE(keyEvent->IsPressed());
+		EXPECT_TRUE((keys_[0].GetKeyCode() !=  keyEvent->GetKeyCode()) && (keyEvent->GetKeyCode() == OHOS::MMI::KeyEvent::KEYCODE_C));
+		EXPECT_EQ(keys_[0].GetDeviceId(), 0);
+		EXPECT_TRUE(keys_[0].IsPressed());
+	}
+    //EXPECT_EQ(keyEvent->GetKeyCode(), OHOS::MMI::KeyEvent::KEYCODE_BACK);
+    EXPECT_EQ(keyEvent->GetKeyAction(), keyActionResult);
+    EXPECT_EQ(keyEvent->GetAction(), keyActionResult);
+    EXPECT_EQ(keyEvent->GetDeviceId(), 0);
+	EXPECT_NE(keyEvent->GetActionTime(), 0);
+	result.push_back(true);
+}
+
+//单击 普通按键
+HWTEST_F(KeyEventTest, MultiModalInputTest_KeyEvent_001, TestSize.Level1)
+{   
+    NUMS = 0;
+	std::string injectionCommand = "hosmmi-event-injection json keyevent001.json";   
+    int32_t response = InputManager::GetInstance()->AddMonitor(KeyClickMonitorCallBack);
+    ASSERT_EQ(MMI_STANDARD_EVENT_SUCCESS, response);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	system(injectionCommand.data());
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	EXPECT_TRUE(result.empty());
+    MMIEventHdl.RemoveInputEventMontior(INDEX_FIRST);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	result.clear();
+	
+}
+
+//普通按下&&抬起
+HWTEST_F(KeyEventTest, MultiModalInputTest_KeyEvent_002, TestSize.Level1)
+{
+	NUMS = 0;
+	std::string injectionCommand = "hosmmi-event-injection json keyevent0020.json";  //单键按下
+    int32_t response = InputManager::GetInstance()->AddMonitor(KeyPressedMonitorCallBack);
+	std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+    ASSERT_EQ(MMI_STANDARD_EVENT_SUCCESS, response);
+	system(injectionCommand.data());
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	EXPECT_TRUE(result.empty());
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	std::string injectionCommand1 = "hosmmi-event-injection json keyevent0021.json";  //单键抬起
+	system(injectionCommand1.data());
+	std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	EXPECT_TRUE(result.empty()&&result.size()==2);
+	MMIEventHdl.RemoveInputEventMontior(INDEX_FIRST);
+	result.clear();
+}
+
+//普通-双击 
+HWTEST_F(KeyEventTest, MultiModalInputTest_KeyEvent_003, TestSize.Level1){
+	NUMS = 0;
+	std::string injectionCommand = "hosmmi-event-injection json keyevent002.json";   //双击
+    int32_t response = InputManager::GetInstance()->AddMonitor(KeyClickMonitorCallBack);
+	std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+    ASSERT_EQ(MMI_STANDARD_EVENT_SUCCESS, response);
+	system(injectionCommand.data());
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	EXPECT_TRUE(result.empty()&&result.size()==4);
+    MMIEventHdl.RemoveInputEventMontior(INDEX_FIRST);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	result.clear();
+}
+
+
+//普通-连击 
+HWTEST_F(KeyEventTest, MultiModalInputTest_KeyEvent_004, TestSize.Level1){
+	NUMS = 0;
+	std::string injectionCommand = "hosmmi-event-injection json keyevent003.json";   //三击
+    int32_t response = InputManager::GetInstance()->AddMonitor(KeyClickMonitorCallBack);
+	std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+    ASSERT_EQ(MMI_STANDARD_EVENT_SUCCESS, response);
+	system(injectionCommand.data());
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	EXPECT_TRUE(result.empty()&&result.size()==6);
+    MMIEventHdl.RemoveInputEventMontior(INDEX_FIRST);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	result.clear();
+}
+
+
+//组合键
+HWTEST_F(KeyEventTest, MultiModalInputTest_KeyEvent_005, TestSize.Level1){
+	NUMS = 0;
+	std::string injectionCommand = "hosmmi-event-injection json keyevent004.json";   //crtl + c
+    int32_t response = InputManager::GetInstance()->AddMonitor(KeyCombinationMonitorCallBack);
+	std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+    ASSERT_EQ(MMI_STANDARD_EVENT_SUCCESS, response);
+	system(injectionCommand.data());
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	EXPECT_TRUE(result.empty()&&result.size()==4);
+    MMIEventHdl.RemoveInputEventMontior(INDEX_FIRST);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP));
+	result.clear();
+}
+}
