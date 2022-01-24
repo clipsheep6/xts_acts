7dd594fc532aafc24e6ddb3881cf474f0ba14c2c
hu-jixiang1 <hujixiang1@huawei.com>
2022-01-24 20:32:51 +0800

    hujixiang1@huawei.com
    Signed-off-by: hu-jixiang1 <hujixiang1@huawei.com>
    Change-Id: I30860b88260fb8ca8fbb85588d014d60815b6e12

---
+++ useriam/face_auth/cpp_api_test/performance_test/include/face_auth_performance_cpp_auto_run_auth_test.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef FACE_AUTH_PERFORMANCE_CPP_AUTO_RUN_AUTH_TEST_H
+#define FACE_AUTH_PERFORMANCE_CPP_AUTO_RUN_AUTH_TEST_H
+#include <thread>
+#include "gtest/gtest.h"
+#include "faceauth_log_wrapper.h"
+#include "constant.h"
+#include "face_auth_defines.h"
+#include "on_faceauth_stub.h"
+
+namespace OHOS {
+namespace UserIAM {
+namespace FaceAuth {
+class FaceAuthPerformanceCppAutoRunAuthTest : public testing::Test {
+public:
+};
+
+class TestCallback : public OnFaceAuthStub {
+public:
+    TestCallback() = default;
+    virtual ~TestCallback() = default;
+
+    void OnCallbackEvent(uint64_t reqId, int32_t type, int32_t code, int32_t errorCode, Bundle &bundle)
+    {
+        reqid_ = reqId;
+        type_ = type;
+        code_ = code;
+        errorcode_ = errorCode;
+        if (code_ == CODE_CALLBACK_RESULT || code_ == CODE_CALLBACK_CANCEL) {
+            isCallback_ = true;
+        }
+        FACEAUTH_LABEL_LOGI("reqid_ is %{public}d", reqid_);
+        FACEAUTH_LABEL_LOGI("type_ is %{public}d", type_);
+        FACEAUTH_LABEL_LOGI("code_ is %{public}d", code_);
+        FACEAUTH_LABEL_LOGI("errorcode_ is %{public}d", errorcode_);
+    }
+    void OnDeathRecipient()
+    {}
+    int32_t reqid_;
+    int32_t type_;
+    int32_t code_;
+    int32_t errorcode_;
+    bool isCallback_ = false;
+};
+}  // namespace FaceAuth
+}  // namespace UserIAM
+}  // namespace OHOS
+#endif  // FACE_AUTH_PERFORMANCE_CPP_AUTO_RUN_AUTH_TEST_H
