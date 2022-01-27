eaa020115a46affa143e9e9139c924d9c29178d8
wangdi119 <wangdi119@huawei.com>
2022-01-27 19:19:06 +0800

    多模 xts js用例申请提交合入
    Signed-off-by: wangdi119 <wangdi119@huawei.com>

--- multimodalinput/input_js_standard/src/main/js/default/test/InputDevice.test.js
+++ multimodalinput/input_js_standard/src/main/js/default/test/InputDevice.test.js
@@ -15,40 +15,77 @@
 
 import inputDevice from '@ohos.multimodalInput.inputDevice';
 import {
-  describe,
-  beforeAll,
-  beforeEach,
-  afterEach,
-  afterAll,
-  it,
-  expect
+	describe,
+	beforeAll,
+	beforeEach,
+	afterEach,
+	afterAll,
+	it,
+	expect
 } from 'deccjsunit/index'
 
 describe('MultimodalInput_test', function () {
+	// 参数正确,返回一个数组
+	it('inputDevice::getDeviceIds_test-01', 0, function () {
+		console.log(`inputDevice::getDeviceIds_test-01 enter`);
+		inputDevice.getDeviceIds((data, err) => {
+			if (err) {
+				expect(false).assertTrue();
+			} else {
+				expect(data).assertInstanceOf('Array');
+			}
+			console.log(`inputDevice::getDeviceIds_test-01 exit`);
+		})
+	})
 
-  // 参数正确,返回一个数组
-  it('inputDevice::getDeviceIds_test-01', 0, function () {
-    console.log(`inputDevice::getDeviceIds_test-01 enter`);
-    inputDevice.getDeviceIds((data, err) => {
-      if (err) {
-        expect(false).assertTrue();
-      } else {
-        expect(data).assertInstanceOf('Array');
-      }
-      console.log(`inputDevice::getDeviceIds_test-01 exit`);
-    })
-  })
+	// 参数正确,判断一种或多种设备
+	it("inputDevice::getDeviceIds_test-02", 0, function () {
+		console.log(`inputDevice::getDeviceIds_test-02 enter`);
+		inputDevice.getDeviceIds((data, err) => {
+			if (err) {
+				expect(false).assertTrue();
+			} else {
+				expect(data.length > 0).assertTure();
+			}
+			console.log(`inputDevice::getDeviceIds_test-02 exit`);
+		})
+	})
 
-  // 参数正确,判断一种或多种设备
-  it("inputDevice::getDeviceIds_test-02", 0, function () {
-    console.log(`inputDevice::getDeviceIds_test-02 enter`);
-    inputDevice.getDeviceIds((data, err) => {
-      if (err) {
-        expect(false).assertTrue();
-      } else {
-        expect(data.length > 0).assertTure();
-      }
-      console.log(`inputDevice::getDeviceIds_test-02 exit`);
-    })
-  })
+	//getdevice
+	// 设备id错误,返回值不是空对象
+	it("inputDevice::getDevice_test-01", 0, function () {
+		console.log(`inputDevice::getDevice_test-03 enter`);
+		inputDevice.getDevice(-1, (data, err) => {
+			if (err) {
+				expect(false).assertTrue();
+				console.log(`inputDevice::getDevice_test-03 ${JSON.stringify(err)}`);
+			} else {
+				expect(JSON.stringify(data) !== "{}").assertTrue();
+			}
+			console.log(`inputDevice::getDevice_test-03 exit`);
+		})
+	})
+
+	// 参数正常,返回值正常
+	it("inputDevice::getDevice_test-02", 0, function () {
+		console.log(`inputDevice::getDevice_test-04 enter`);
+		inputDevice.getDeviceIds((data, err) => {
+			if (err) {
+				expect(false).assertTrue();
+			} else {
+				let arr = [];
+				for (var i = 0; i < data.length; i++) {
+					inputDevice.getDevice(data[i], (res, err) => {
+						console.log(`getDevice:data_i ${JSON.stringify(data[i])}`)
+						console.log(`getDevice:data ${JSON.stringify(data)}`)
+						console.log(`getDevice:err ${JSON.stringify(err)}`)
+						arr = Object.keys(res);
+					})
+				}
+				expect(arr.length > 0).assertTrue();
+			}
+			console.log(`inputDevice::getDevice_test-04 exit`);
+		});
+	})
+
 })
