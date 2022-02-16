1ff00d0a60a0b9f5ce72bc4e90b4cda69b0dd62c
zxg-gitee <zhaoxiaoguang2@huawei.com>
2022-02-16 11:07:42 +0000

    update graphic/webGL/src/main/js/default/pages/index/index.js.

--- graphic/webGL/src/main/js/default/pages/index/index.js
+++ graphic/webGL/src/main/js/default/pages/index/index.js
@@ -22,26 +22,20 @@ export default {
         this.title = this.$t('strings.world');
     },
     onShow(){
-        // console.info("webGL_test start1");
         global.el = this.$refs.canvas1;
         global.el2 = this.$refs.canvas2;
-        // global.a = this.$element("canvas1")
         
         const core = Core.getInstance()
         const expectExtend = new ExpectExtend({
             'id':'extend'
         })
-        // console.info("webGL_test start2");
         core.addService('expect',expectExtend)
         core.init()
-        // console.info("webGL_test start3");
         const configService = core.getDefaultService('config')
         this.timeout = 60000
         configService.setConfig(this)
-        // console.info("webGL_test start4");
         require('../../test/List.test.js')
         core.execute()
-        // console.info("webGL_test start5");
 
     }
 }
