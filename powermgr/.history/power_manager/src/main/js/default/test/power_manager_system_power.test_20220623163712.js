6e0cd0cdb61860e8a7ce97fd4c6703d329290bfe
liuzixuan <liuzixuan10@huawei.com>
2022-06-23 17:13:11 +0800

    add power and battery
    Signed-off-by: liuzixuan <liuzixuan10@huawei.com>
    Change-Id: I91f02c2bbf3308948b72f2002f49146d36f55d71

--- powermgr/.history/power_manager/src/main/js/default/test/power_manager_system_power.test_20220623163712.js
+++ powermgr/.history/power_manager/src/main/js/default/test/power_manager_system_power.test_20220623163712.js
@@ -14,6 +14,7 @@
  */
 
 import power from '@ohos.power';
+import 'ohos.permission.POWER_REBOOT'
 
 import { describe, beforeAll, beforeEach, afterEach, afterAll, it, expect } from 'deccjsunit/index'
 
