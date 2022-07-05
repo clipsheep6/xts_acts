952b85a327d3fcbc9ae2653972a4ee711e47e1b9
NOBUGGERS <ruanmeng@huawei.com>
2022-07-05 06:44:59 +0000

    add 2.4.5 add 2.4.5

---
+++ multimedia/media/media_js_standard/videoRecorder/src/main/js/test/List.test.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+require('./VideoRecorderSpliterCallbackTest.test.js') 
+require('./VideoRecorderSpliterCallbackAbnormalTest.test.js')  
+require('./VideoRecorderSpliterCallbackStabilityTest.test.js')
+require('./VideoRecorderSpliterPromiseTest.test.js')  
+require('./VideoRecorderSpliterPromiseAbnormalTest.test.js')  
+require('./VideoRecorderSpliterPromiseStabilityTest.test.js')  
+//require('./VideoRecorderFuncCallbackTest.test.js')
+//require('./VideoRecorderAPICallbackTest.test.js')
+//require('./VideoRecorderEnumTest.test.js')
+//require('./VideoRecorderFuncCallbackTest.test.js')
+//require('./VideoRecorderAVCFuncCallbackTest.test.js')
+//require('./VideoRecorderAVCFuncPromiseTest.test.js')
+
+
