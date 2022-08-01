a5fcfd9a8ba1800360eed434e4c9a026ddf80601
wangtiantian <wangtiantian19@huawei.com>
2022-08-01 09:25:05 +0800

    IssueNo: #I5JSUM:add complete Description:add complete Sig:SIG_ApplicaitonFramework Feature or Bugfix:Bugfix Binary Source:No Signed-off-by: wangtiantian <wangtiantian19@huawei.com>

--- bundlemanager/bundle_standard/bundlemanager/actsbmsjstest/entry/src/main/js/test/ActsBmsHasInstalldTest.test.js
+++ bundlemanager/bundle_standard/bundlemanager/actsbmsjstest/entry/src/main/js/test/ActsBmsHasInstalldTest.test.js
@@ -28,10 +28,12 @@ describe('ActsBmsHasInstalldTest', function () {
      * @tc.desc Test hasInstalled interface.
      */
     it('hasInstalled_0100', 0, async function (done) {
+        let flag = 0;
         pkg.hasInstalled({
             bundleName: 'com.ohos.launcher',
             success: function success(data) {
                 console.info('hasInstalled success function in');
+                flag += 1;
                 expect(data.result).assertTrue();
                 done();
             },
@@ -42,7 +44,7 @@ describe('ActsBmsHasInstalldTest', function () {
             },
             complete: function complete() {
                 console.info('hasInstalled complete function in');
-                expect().assertFail();
+                expect(flag).assertEqual(1);
                 done();
             }
         });
@@ -54,10 +56,12 @@ describe('ActsBmsHasInstalldTest', function () {
      * @tc.desc Test hasInstalled interface.
      */
     it('hasInstalled_0200', 0, async function (done) {
+        let flag = 0;
         pkg.hasInstalled({
             bundleName: 'wrongName',
             success: function success(data) {
                 console.info('hasInstalled success function in');
+                flag += 1;
                 expect(data.result).assertFalse();
                 done();
             },
@@ -68,7 +72,7 @@ describe('ActsBmsHasInstalldTest', function () {
             },
             complete: function complete() {
                 console.info('hasInstalled complete function in');
-                expect().assertFail();
+                expect(flag).assertEqual(1);
                 done();
             }
         });
