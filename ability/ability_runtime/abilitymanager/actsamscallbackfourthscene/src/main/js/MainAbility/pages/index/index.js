41274e9a278f4494e7a19063f5c906ad65ab8644
wangfandev <fanwangah@isoftstone.com>
2022-08-05 10:08:42 +0800

    Signed-off-by: wangfandev <fanwangah@isoftstone.com> 修改编码规范问题

--- ability/ability_runtime/abilitymanager/actsamscallbackfourthscene/src/main/js/MainAbility/pages/index/index.js
+++ ability/ability_runtime/abilitymanager/actsamscallbackfourthscene/src/main/js/MainAbility/pages/index/index.js
@@ -21,7 +21,7 @@ export default {
         title: ''
     },
     onInit() {
-        this.title = this.$t('strings.world');
+        this.title = this.$t('strings.world')
     },
     onShow() {
         if (once) {
@@ -30,7 +30,7 @@ export default {
         }
     },
     onReady() {
-        console.info('onReady');
+        console.info('onReady')
     },
 }
 
