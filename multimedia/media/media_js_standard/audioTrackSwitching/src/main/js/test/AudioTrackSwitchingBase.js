99b7d6b2f2f43b146dc5b7e793e6ca8809d29601
NOBUGGERS <ruanmeng@huawei.com>
2022-08-11 08:50:13 +0000

    add 2.4.11 selectTrack add 2.4.11 selectTrack

---
+++ multimedia/media/media_js_standard/audioTrackSwitching/src/main/js/test/AudioTrackSwitchingBase.js
@@ -0,0 +1,282 @@
+// @ts-nocheck
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const AAC_3TRK_MKV_SOURCE = [
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 0,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 1,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 2,
+        'track_type': 0
+    }
+];
+
+export const MP3_3TRK_MKV_SOURCE = [
+    {
+        'channel_count': 1,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 48000,
+        'track_index': 0,
+        'track_type': 0
+    },
+    {
+        'channel_count': 1,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 48000,
+        'track_index': 1,
+        'track_type': 0
+    },
+    {
+        'channel_count': 1,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 48000,
+        'track_index': 2,
+        'track_type': 0
+    }
+];
+
+export const AAC_2TRK_MP3_1TRK_MKV_SOURCE = [
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 0,
+        'track_type': 0
+    },
+    {
+        'channel_count': 1,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 48000,
+        'track_index': 1,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 2,
+        'track_type': 0
+    }
+];
+
+export const AAC_16TRK_MKV_SOURCE = [
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 0,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 1,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 2,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 3,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 4,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 5,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 6,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 7,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 8,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 9,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 10,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 11,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 12,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 13,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 14,
+        'track_type': 0
+    },
+    {
+        'channel_count': 2,
+        'codec_mime': 'audio/mpeg',
+        'language_code': 'und',
+        'sample_rate': 44100,
+        'track_index': 15,
+        'track_type': 0
+    }
+];
+
+export const AAC_3TRK_TS_SOURCE = [
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 0,
+        'track_type': 0
+    },
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 1,
+        'track_type': 0
+    },
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 2,
+        'track_type': 0
+    }
+];
+
+export const MP3_3TRK_TS_SOURCE = [
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 0,
+        'track_type': 0
+    },
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 1,
+        'track_type': 0
+    },
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 2,
+        'track_type': 0
+    }
+];
+
+export const AAC_2TRK_MP3_1TRK_TS_SOURCE = [
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 0,
+        'track_type': 0
+    },
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 1,
+        'track_type': 0
+    },
+    {
+        'codec_mime': 'audio/mpeg',
+        'track_index': 2,
+        'track_type': 0
+    }
+];
+
