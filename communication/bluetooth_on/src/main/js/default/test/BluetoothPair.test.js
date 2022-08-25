269f6a8ab8bd6fda8f85317a5626b57ec6a2f2c4
15929983503 <duanhaizhen@huawei.com>
2022-08-25 16:35:27 +0800

    Signed-off-by:small_tina_yang<yangcuicui2@huawei.com>
    Signed-off-by: 15929983503 <duanhaizhen@huawei.com>

--- communication/bluetooth_on/src/main/js/default/test/BluetoothPair.test.js
+++ communication/bluetooth_on/src/main/js/default/test/BluetoothPair.test.js
@@ -225,7 +225,7 @@ describe('bluetoothhostTest2', function() {
         bluetooth.BLE.on('pinRequired', PinRequiredParam);
         let result = bluetooth.pairDevice("00:00:00:00:00:00");
         console.info("[bluetooth_js] onStartpair007 -> " + JSON.stringify(result));
-        expect(result).assertTrue();
+        expect(result).assertFalse();
         bluetooth.BLE.off('pinRequired', PinRequiredParam);
         done()
     })
@@ -247,7 +247,7 @@ describe('bluetoothhostTest2', function() {
         bluetooth.BLE.on('pinRequired', PinRequiredParam);
         let result = bluetooth.pairDevice("00:00:00:00:00:00");
         console.info("[bluetooth_js] onStartpair008 -> " + JSON.stringify(result));
-        expect(result).assertTrue();
+        expect(result).assertFalse();
         bluetooth.BLE.off('pinRequired', PinRequiredParam);
         done()
     })
