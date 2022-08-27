4f16ff02110435d8afb8c1d7d696ec334073182f
15929983503 <duanhaizhen@huawei.com>
2022-08-27 10:24:04 +0800

    Signed-off-by:small_tina_yang<yangcuicui2@huawei.com>
    Signed-off-by: 15929983503 <duanhaizhen@huawei.com>

--- communication/bluetooth_ble/src/main/js/default/test/List.test.js
+++ communication/bluetooth_ble/src/main/js/default/test/List.test.js
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-require('./BluetoothBle.test.js') 
 require('./BleAdvertiser.test.js')
 require('./BleGattManager.test.js')
 require('./BleService.test')
