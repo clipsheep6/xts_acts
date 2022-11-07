75b6a69dfbccde95449e8db0675ac1b2d6e5b749
chenkang <chenkang85@huawei.com>
2022-11-07 22:47:42 +0800

    update beizhu2
    Signed-off-by: chenkang <chenkang85@huawei.com>

--- powermgr/display_manager/src/main/js/test/system_display.test.js
+++ powermgr/display_manager/src/main/js/test/system_display.test.js
@@ -11,6 +11,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * mark one
+ * mark two
+ * mark three
 */
 import brightness from '@system.brightness';
 import { describe, it, expect } from '@ohos/hypium';
