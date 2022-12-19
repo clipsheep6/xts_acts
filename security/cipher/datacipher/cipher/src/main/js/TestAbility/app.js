497d0b6173f12a10a09d03016688dfa2bf35392c
jianjew <liaojianjun1@huawei.com>
2022-12-19 01:35:40 +0000

    update security/cipher/datacipher/cipher/src/main/js/TestAbility/app.js.
    Signed-off-by: jianjew <liaojianjun1@huawei.com>

--- security/cipher/datacipher/cipher/src/main/js/TestAbility/app.js
+++ security/cipher/datacipher/cipher/src/main/js/TestAbility/app.js
@@ -22,7 +22,7 @@ export default {
         console.info('TestApplication onCreate')
         var abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
         var abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
-        console.info('start run testcase!!!')
+        console.info('start run testcase!!!!')
         Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite)
     },
     onDestroy() {
