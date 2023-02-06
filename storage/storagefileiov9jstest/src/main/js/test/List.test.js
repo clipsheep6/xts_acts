4baef8f38f04194d81b59caf9a856438a7f808f1
zhuhongtao66 <zhuhongtao12@huawei.com>
2023-02-06 10:42:03 +0800

    fixed 9964d4e from https://gitee.com/zhuhongtao66/xts_acts_mod_fs_xts/pulls/7423 Add XTS for moveFile interface of mod_fs
    Signed-off-by: zhuhongtao66 <zhuhongtao12@huawei.com>

--- storage/storagefileiov9jstest/src/main/js/test/List.test.js
+++ storage/storagefileiov9jstest/src/main/js/test/List.test.js
@@ -13,12 +13,14 @@
  * limitations under the License.
  */
 
+import fileIOMoveFile from './members/moveFile.test.js'
 import fileIOOpen from './members/open.test.js'
 import fileIORead from './members/read.test.js'
 import fileIOStat from './members/stat.test.js'
 import fileIOTruncate from './members/truncate.test.js'
 import fileIOWrite from './members/write.test.js'
 export default function testsuite() {
+    fileIOMoveFile()
     fileIOOpen()
     fileIORead()
     fileIOStat()
