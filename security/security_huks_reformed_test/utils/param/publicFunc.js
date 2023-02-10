02a0426c6083148ba0bdc9fdda27a0adb5b2282c
nozomi <zhanghanlin9@huawei.com>
2023-02-10 14:15:04 +0800

    shipeishebei
    Signed-off-by: nozomi <zhanghanlin9@huawei.com>

--- security/security_huks_reformed_test/utils/param/publicFunc.js
+++ security/security_huks_reformed_test/utils/param/publicFunc.js
@@ -13,26 +13,26 @@
  * limitations under the License.
  */
 function stringToArray(str) {
-  var arr = [];
-  for (var i = 0, j = str.length; i < j; ++i) {
+  let arr = [];
+  for (let i = 0, j = str.length; i < j; ++i) {
     arr.push(str.charCodeAt(i));
   }
   return arr;
 }
 
 function uint8ArrayToString(fileData) {
-  var dataString = '';
-  for (var i = 0; i < fileData.length; i++) {
+  let dataString = '';
+  for (let i = 0; i < fileData.length; i++) {
     dataString += String.fromCharCode(fileData[i]);
   }
   return dataString;
 }
 function stringToUint8Array(str) {
-  var arr = [];
-  for (var i = 0, j = str.length; i < j; ++i) {
+  let arr = [];
+  for (let i = 0, j = str.length; i < j; ++i) {
     arr.push(str.charCodeAt(i));
   }
-  var tmpUint8Array = new Uint8Array(arr);
+  let tmpUint8Array = new Uint8Array(arr);
   return tmpUint8Array;
 }
 function arrayEqual(a, b) {
