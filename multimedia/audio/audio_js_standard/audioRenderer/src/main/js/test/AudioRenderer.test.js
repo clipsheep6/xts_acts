20f83144f57627dccbf518e15a27d5e5d31a80ed
zhuchaochao <zhuchaochao2@huawei.com>
2023-03-15 14:46:43 +0800

    fix interrupt
    Signed-off-by: zhuchaochao <zhuchaochao2@huawei.com>

--- multimedia/audio/audio_js_standard/audioRenderer/src/main/js/test/AudioRenderer.test.js
+++ multimedia/audio/audio_js_standard/audioRenderer/src/main/js/test/AudioRenderer.test.js
@@ -2016,7 +2016,6 @@ describe('audioRenderer', function () {
                 console.info("AudioFrameworkRenderLog: InterruptHint : " + interruptEvent.hintType);
                 if (interruptEvent.hintType >= 0) {
                     console.info("AudioFrameworkRenderLog: on'audioInterrupt' SUCCESS ");
-                    interrput_flag = true;
                 }
             });
 
