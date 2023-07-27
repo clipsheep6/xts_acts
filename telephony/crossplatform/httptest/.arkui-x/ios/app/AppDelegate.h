b5ff5fe27a2db69df7f8a3d97eeb3ea819dd5a7a
huyanqiang <huyanqiang5@huawei.com>
2023-07-27 19:27:10 +0800

    codecheck
    Signed-off-by: huyanqiang <huyanqiang5@huawei.com>

--- telephony/crossplatform/httptest/.arkui-x/ios/app/AppDelegate.h
+++ telephony/crossplatform/httptest/.arkui-x/ios/app/AppDelegate.h
@@ -13,6 +13,8 @@
  * limitations under the License.
  */
 
+#ifndef REQUEST_CROSSPLATFORM_REQUESTTEST__ARKUI_X_IOS_APP_APPDELEGATE_H 
+#define REQUEST_CROSSPLATFORM_REQUESTTEST__ARKUI_X_IOS_APP_APPDELEGATE_H
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
@@ -20,4 +22,4 @@
 @property (nonatomic, strong) UIWindow *window;
 
 @end
-
+#endif /* REQUEST_CROSSPLATFORM_REQUESTTEST__ARKUI_X_IOS_APP_APPDELEGATE_H  */
