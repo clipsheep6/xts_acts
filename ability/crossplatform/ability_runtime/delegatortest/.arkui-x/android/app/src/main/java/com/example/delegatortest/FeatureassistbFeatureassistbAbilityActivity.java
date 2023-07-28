add018639a9f136cab905c4c0d8a870378466868
huyanqiang5@huawei.com <huyanqiang5@huawei.com>
2023-07-28 21:45:22 +0800

    完成delegatortest的跨平台测试套归一化
    Signed-off-by: huyanqiang5@huawei.com <huyanqiang5@huawei.com>

---
+++ ability/crossplatform/ability_runtime/delegatortest/.arkui-x/android/app/src/main/java/com/example/delegatortest/FeatureassistbFeatureassistbAbilityActivity.java
@@ -0,0 +1,23 @@
+package com.example.delegatortest;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import ohos.stage.ability.adapter.StageActivity;
+
+
+/**
+ * Example ace activity class, which will load ArkUI-X ability instance.
+ * StageActivity is provided by ArkUI-X
+ * @see <a href=
+ * "https://gitee.com/arkui-crossplatform/doc/blob/master/contribute/tutorial/how-to-build-Android-app.md">
+ * to build android library</a>
+ */
+public class FeatureassistbFeatureassistbAbilityActivity extends StageActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e("HiHelloWorld", "FeatureassistbFeatureassistbAbilityActivity");
+        setInstanceName("com.example.delegatortest:featureassistb:FeatureassistbAbility:");
+        super.onCreate(savedInstanceState);
+    }
+}
