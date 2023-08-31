fdd505a1f3393fea0e138a85b695355e26302c1d
fengjq <fengjunqing@huawei.com>
2023-08-31 11:19:12 +0800

    add recent xts
    Signed-off-by: fengjq <fengjunqing@huawei.com>

---
+++ storage/storagerecentEtstest/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -0,0 +1,35 @@
+import Ability from '@ohos.app.ability.UIAbility'
+
+export default class MainAbility extends Ability {
+    onCreate(want, launchParam) {
+        // Ability is creating, initialize resources for this ability
+        console.log("[Demo] MainAbility onCreate")
+    }
+
+    onDestroy() {
+        // Ability is destroying, release resources for this ability
+        console.log("[Demo] MainAbility onDestroy")
+    }
+
+    async onWindowStageCreate(windowStage) {
+        console.log("[Demo] MainAbility onWindowStageCreate windowStage=" + windowStage)
+        globalThis.abilityContext = this.context;
+        globalThis.mainAbilityResourceManager = this.context.resourceManager;
+        windowStage.setUIContent(this.context, "MainAbility/pages/index/index", null)
+    }
+
+    onWindowStageDestroy() {
+        //Main window is destroyed, release UI related resources
+        console.log("[Demo] MainAbility onWindowStageDestroy")
+    }
+
+    onForeground() {
+        // Ability has brought to foreground
+        console.log("[Demo] MainAbility onForeground")
+    }
+
+    onBackground() {
+        // Ability has back to background
+        console.log("[Demo] MainAbility onBackground")
+    }
+};
