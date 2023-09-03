8da249f4812f8330a35d87063528a693f912ce9d
quanli <quanli1@huawei.com>
2023-09-03 10:52:38 +0800

    quanli1@huawei.com
    Signed-off-by: quanli <quanli1@huawei.com>

--- communication/bluetooth_profile/src/main/js/test/BtManagerA2dp.test.js
+++ communication/bluetooth_profile/src/main/js/test/BtManagerA2dp.test.js
@@ -15,6 +15,7 @@
 
 import bluetoothManager from '@ohos.bluetoothManager';
 import {describe, beforeAll, beforeEach, afterEach, afterAll, it, expect} from '@ohos/hypium'
+import { UiComponent, UiDriver, BY, Component, Driver, UiWindow, ON, MatchPattern, DisplayRotation, ResizeDirection, UiDirection, MouseButton, WindowMode, PointerMatrix, UIElementInfo, UIEventObserver } from '@ohos.UiTest'
 
 export default function btManagerA2dpConnTest() {
 describe('btManagerA2dpConnTest', function() {
@@ -22,11 +23,29 @@ describe('btManagerA2dpConnTest', function() {
         return new Promise(resovle => setTimeout(resovle, delay))
     }
 
+    async function clickTheWindow() {
+        try{
+            let driver = Driver.create();
+            console.info('[bluetooth_js] bt driver create:'+ driver);            
+            await driver.delayMs(1000);
+            await driver.wakeUpDisplay();
+            await driver.delayMs(3000);
+            await driver.swipe(1500, 1000, 1500, 100);
+            await driver.delayMs(3000);
+            await driver.click(950, 2550);
+            await driver.delayMs(1500);
+            await driver.click(950, 2550);
+        } catch (error) {
+            console.info('[bluetooth_js] driver error info:'+ error);
+        }
+    }
+
     async function tryToEnableBt() {
         let sta = bluetoothManager.getState();
         switch (sta) {
             case 0:
                 bluetoothManager.enableBluetooth();
+                clickTheWindow();
                 await sleep(10000);
                 let sta1 = bluetoothManager.getState();
                 console.info('[bluetooth_js] bt turn off:' + JSON.stringify(sta1));
@@ -40,6 +59,7 @@ describe('btManagerA2dpConnTest', function() {
                 break;
             case 3:
                 bluetoothManager.enableBluetooth();
+                clickTheWindow();
                 await sleep(10000);
                 let sta2 = bluetoothManager.getState();
                 console.info('[bluetooth_js] bt turning off:' + JSON.stringify(sta2));
--- communication/btmanager_errorcode401/src/main/js/test/BtMGattManager401.test.js
+++ communication/btmanager_errorcode401/src/main/js/test/BtMGattManager401.test.js
@@ -16,6 +16,7 @@
 //import bluetooth from '@ohos.bluetooth';
 import bluetooth from '@ohos.bluetoothManager';
 import {describe, beforeAll, beforeEach, afterEach, afterAll, it, expect} from '@ohos/hypium'
+import { UiComponent, UiDriver, BY, Component, Driver, UiWindow, ON, MatchPattern, DisplayRotation, ResizeDirection, UiDirection, MouseButton, WindowMode, PointerMatrix, UIElementInfo, UIEventObserver } from '@ohos.UiTest'
 
 export default function btManagerMError401Test() {
 describe('btManagerMError401Test', function() {
@@ -24,12 +25,30 @@ describe('btManagerMError401Test', function() {
         return new Promise(resovle => setTimeout(resovle, delay))
     }
 
+    async function clickTheWindow() {
+        try{
+            let driver = Driver.create();
+            console.info('[bluetooth_js] bt driver create:'+ driver);            
+            await driver.delayMs(1000);
+            await driver.wakeUpDisplay();
+            await driver.delayMs(3000);
+            await driver.swipe(1500, 1000, 1500, 100);
+            await driver.delayMs(3000);
+            await driver.click(950, 2550);
+            await driver.delayMs(1500);
+            await driver.click(950, 2550);
+        } catch (error) {
+            console.info('[bluetooth_js] driver error info:'+ error);
+        }
+    }
+
     async function tryToEnableBt() {
         let sta = bluetooth.getState();
         switch(sta){
             case 0:
                 console.info('[bluetooth_js] bt turn off:'+ JSON.stringify(sta));
                 bluetooth.enableBluetooth();
+                clickTheWindow();
                 await sleep(10000);
                 break;
             case 1:
@@ -42,6 +61,7 @@ describe('btManagerMError401Test', function() {
             case 3:
                 console.info('[bluetooth_js] bt turning off:'+ JSON.stringify(sta));
                 bluetooth.enableBluetooth();
+                clickTheWindow();
                 await sleep(10000);
                 break;
             default:
