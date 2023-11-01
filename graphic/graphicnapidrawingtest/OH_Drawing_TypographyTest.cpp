c0b2447b0622850646c6793a96ff57ddd1a42bee
changleipeng <changleipeng4@huawei.com>
2023-11-01 17:09:30 +0800

    解决作用域相同导致xts编译异常问题
    Signed-off-by: changleipeng <changleipeng4@huawei.com>

--- graphic/graphicnapidrawingtest/OH_Drawing_TypographyTest.cpp
+++ graphic/graphicnapidrawingtest/OH_Drawing_TypographyTest.cpp
@@ -433,21 +433,21 @@ HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest014, TestSize.Level
     OH_Drawing_TextStyle* txtStyle = OH_Drawing_CreateTextStyle();
     OH_Drawing_SetTextStyleFontStyle(txtStyle, FONT_STYLE_NORMAL);
 #ifndef USE_GRAPHIC_TEXT_GINE
-    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle_, FontStyle::NORMAL);
+    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle_, rosen::FontStyle::NORMAL);
 #else
-    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle, FontStyle::NORMAL);
+    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle, rosen::FontStyle::NORMAL);
 #endif
     OH_Drawing_SetTextStyleFontStyle(txtStyle, FONT_STYLE_ITALIC);
 #ifndef USE_GRAPHIC_TEXT_GINE
-    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle_, FontStyle::ITALIC);
+    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle_, rosen::FontStyle::ITALIC);
 #else
-    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle, FontStyle::ITALIC);
+    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle, rosen::FontStyle::ITALIC);
 #endif
     OH_Drawing_SetTextStyleFontStyle(txtStyle, -1);
 #ifndef USE_GRAPHIC_TEXT_GINE
-    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle_, FontStyle::NORMAL);
+    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle_, rosen::FontStyle::NORMAL);
 #else
-    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle, FontStyle::NORMAL);
+    EXPECT_EQ(ConvertToOriginalText(txtStyle)->fontStyle, rosen::FontStyle::NORMAL);
 #endif
 }
 
