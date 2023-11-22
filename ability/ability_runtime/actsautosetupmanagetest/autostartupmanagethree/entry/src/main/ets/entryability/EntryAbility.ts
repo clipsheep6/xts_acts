45d7d92b59a07d56bd3a1a370a4ce1a4d14bd5c4
xinking129 <xinxin13@huawei.com>
2023-11-22 12:42:35 +0800

    checkCode
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- ability/ability_runtime/actsautosetupmanagetest/autostartupmanagethree/entry/src/main/ets/entryability/EntryAbility.ts
+++ ability/ability_runtime/actsautosetupmanagetest/autostartupmanagethree/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,10 +1,24 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 import AutoStartUpManager from '@ohos.app.ability.autoStartupManager';
 import commonEvent from '@ohos.commonEventManager';
 
-function Acts_AutoStartup_1600() {
+function autoStartup1600() {
   console.info('====>   Three  Acts_AutoStartup_1600');
   try {
     AutoStartUpManager.setApplicationAutoStartup({
@@ -21,10 +35,10 @@ function Acts_AutoStartup_1600() {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_UI_SET', commonEventData, (err) => {
       console.info('====> autostartupmanagethree ACTS_CALL_EVENT_UI_SET publish: ' + JSON.stringify(err));
-    })
+    });
   }
 
   try {
@@ -42,14 +56,14 @@ function Acts_AutoStartup_1600() {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_UI_CANCEL', commonEventData, (err) => {
       console.info('====> autostartupmanagetwo ACTS_CALL_EVENT_OFF publish: ' + JSON.stringify(err));
-    })
+    });
   }
 }
 
-function Acts_AutoStartup_1700() {
+function autoStartup1700() {
   try {
     AutoStartUpManager.setApplicationAutoStartup({
       bundleName: 'com.acts.autostartupapp',
@@ -66,11 +80,11 @@ function Acts_AutoStartup_1700() {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_UI_SET', commonEventData, (err) => {
       console.info('====> autostartupmanagethree Acts_AutoStartup_1700 ACTS_CALL_EVENT_UI_SET publish: ' +
       JSON.stringify(err));
-    })
+    });
   }
 
   try {
@@ -89,37 +103,34 @@ function Acts_AutoStartup_1700() {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_UI_CANCEL', commonEventData, (err) => {
       console.info('====> autostartupmanagethree Acts_AutoStartup_1700 ACTS_CALL_EVENT_UI_CANCEL publish: ' +
       JSON.stringify(err));
-    })
+    });
   }
-
-
 }
 
-function Acts_AutoStartup_1800() {
+function autoStartup1800() {
 
   try {
     AutoStartUpManager.queryAllAutoStartupApplications((err, data) => {
       console.info('====> Acts_AutoStartup_1800 queryAllAutoStartupApplications2 OK');
-    })
+    });
   } catch (err) {
     let commonEventData = {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_QUERY', commonEventData, (err) => {
       console.info('====> autostartupmanagethree Acts_AutoStartup_1800 ACTS_CALL_EVENT_QUERY publish: ' +
       JSON.stringify(err));
-    })
+    });
   }
-
 }
 
-function Acts_AutoStartup_1900() {
+function autoStartup1900() {
 
   try {
     AutoStartUpManager.queryAllAutoStartupApplications().then((data) => {
@@ -127,21 +138,21 @@ function Acts_AutoStartup_1900() {
     }).catch((err) => {
       console.info('====> autostartupmanagethree Acts_AutoStartup_1900 queryAllAutoStartupApplications1 ERR err: ' +
       JSON.stringify(err));
-    })
+    });
   } catch (err) {
     let commonEventData = {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_QUERY', commonEventData, (err) => {
       console.info('====> autostartupmanagethree Acts_AutoStartup_1900 ACTS_CALL_EVENT_QUERY publish: ' +
       JSON.stringify(err));
-    })
+    });
   }
 }
 
-function Acts_AutoStartup_2000() {
+function autoStartup2000() {
   let AutoStartUpCallback = {
     onAutoStartupOn(data) {
       console.info('===> autostartupmanagethree Acts_AutoStartup_2000 onAutoStartupOn data: ' + JSON.stringify(data));
@@ -151,7 +162,7 @@ function Acts_AutoStartup_2000() {
     }
   }
   try {
-    AutoStartUpManager.on('systemAutoStartup', AutoStartUpCallback)
+    AutoStartUpManager.on('systemAutoStartup', AutoStartUpCallback);
   } catch (err) {
     console.info('====> catch autostartupmanagethree Acts_AutoStartup_2000 ACTS_CALL_EVENT_QUERY ON publish: ' +
     JSON.stringify(err));
@@ -159,15 +170,15 @@ function Acts_AutoStartup_2000() {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_REGISTER_ON', commonEventData, (err) => {
       console.info('====> autostartupmanagethree Acts_AutoStartup_2000 ACTS_CALL_EVENT_QUERY ON publish: ' +
       JSON.stringify(err));
-    })
+    });
   }
 
   try {
-    AutoStartUpManager.off('systemAutoStartup', AutoStartUpCallback)
+    AutoStartUpManager.off('systemAutoStartup', AutoStartUpCallback);
   } catch (err) {
     console.info('====> catch autostartupmanagethree Acts_AutoStartup_2000 ACTS_CALL_EVENT_QUERY OFF publish: ' +
     JSON.stringify(err));
@@ -175,11 +186,11 @@ function Acts_AutoStartup_2000() {
       parameters: {
         str: err.code
       }
-    }
+    };
     commonEvent.publish('ACTS_CALL_EVENT_REGISTER_OFF', commonEventData, (err) => {
       console.info('====> autostartupmanagethree Acts_AutoStartup_2000 ACTS_CALL_EVENT_QUERY OFF publish: ' +
       JSON.stringify(err));
-    })
+    });
   }
 }
 
@@ -188,33 +199,29 @@ export default class EntryAbility extends UIAbility {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
 
     if (want.action === 'Acts_AutoStartup_1600') {
-      console.info("if (want.action === 'Acts_AutoStartup_1600')")
-      Acts_AutoStartup_1600();
+      autoStartup1600();
     } else if (want.action === 'Acts_AutoStartup_1700') {
-      Acts_AutoStartup_1700();
+      autoStartup1700();
     } else if (want.action === 'Acts_AutoStartup_1800') {
-      console.info("if want (want.action === 'Acts_AutoStartup_1600')")
-      Acts_AutoStartup_1800();
+      autoStartup1800();
     } else if (want.action === 'Acts_AutoStartup_1900') {
-      Acts_AutoStartup_1900();
+      autoStartup1900();
     } else if (want.action === 'Acts_AutoStartup_2000') {
-      Acts_AutoStartup_2000();
+      autoStartup2000();
     }
   }
 
   onNewWant(want) {
     if (want.action === 'Acts_AutoStartup_1600') {
-      console.info("if (want.action === 'Acts_AutoStartup_1600')")
-      Acts_AutoStartup_1600();
+      autoStartup1600();
     } else if (want.action === 'Acts_AutoStartup_1700') {
-      Acts_AutoStartup_1700();
+      autoStartup1700();
     } else if (want.action === 'Acts_AutoStartup_1800') {
-      console.info("if newwant (want.action === 'Acts_AutoStartup_1600')")
-      Acts_AutoStartup_1800();
+      autoStartup1800();
     } else if (want.action === 'Acts_AutoStartup_1900') {
-      Acts_AutoStartup_1900();
+      autoStartup1900();
     } else if (want.action === 'Acts_AutoStartup_2000') {
-      Acts_AutoStartup_2000();
+      autoStartup2000();
     }
   }
 
