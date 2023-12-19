7304a6256206843dc18f3998f779b6a81d2ee525
yuxuewen1 <yuxuewen1@h-partners.com>
2023-12-19 21:10:28 +0800

    RM003删除多余注释代码

--- graphic/crossplatform/windowsubsystem/entry/src/main/ets/Mainability/Mainability.ts
+++ graphic/crossplatform/windowsubsystem/entry/src/main/ets/Mainability/Mainability.ts
@@ -4,8 +4,6 @@ import window from '@ohos.window';
 export default class MainAbility extends UIAbility {
   onCreate(want, launchParam) {
     console.log('testTag', '%{public}s', 'Ability onCreate');
-    // console.log('MainAbility','55555')
-    // globalThis.context_EntryMainAbility = this.context
   }
 
   onDestroy() {
@@ -13,7 +11,6 @@ export default class MainAbility extends UIAbility {
   }
 
   onWindowStageCreate(windowStage: window.WindowStage) {
-    // Main window is created, set main page for this ability
     console.log('testTag', '%{public}s', 'Ability onWindowStageCreate');
 
 
@@ -28,24 +25,21 @@ export default class MainAbility extends UIAbility {
 
     try {
       let windowMainAbility = windowStage.getMainWindowSync();
-      AppStorage.setOrCreate('windowMainAbility',windowMainAbility)
+      AppStorage.setOrCreate('windowMainAbility', windowMainAbility)
     } catch (exception) {
       console.error('Failed to obtain the main windowMainAbility. Cause: ' + JSON.stringify(exception));
     }
   }
 
   onWindowStageDestroy() {
-    // Main window is destroyed, release UI related resources
     console.log('testTag', '%{public}s', 'Ability onWindowStageDestroy');
   }
 
   onForeground() {
-    // Ability has brought to foreground
     console.log('testTag', '%{public}s', 'Ability onForeground');
   }
 
   onBackground() {
-    // Ability has back to background
     console.log('testTag', '%{public}s', 'Ability onBackground');
   }
 }
