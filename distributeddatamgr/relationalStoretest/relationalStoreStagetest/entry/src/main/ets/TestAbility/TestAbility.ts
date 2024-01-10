ef8e89f3fa6ad44385931768d4ed2c03dc1d46c0
guotao <guotao76@huawei.com>
2024-01-10 11:17:54 +0800

    <update>
    Signed-off-by: guotao <guotao76@huawei.com>

--- distributeddatamgr/relationalStoretest/relationalStoreStagetest/entry/src/main/ets/TestAbility/TestAbility.ts
+++ distributeddatamgr/relationalStoretest/relationalStoreStagetest/entry/src/main/ets/TestAbility/TestAbility.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -7,44 +7,58 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Ability from '@ohos.app.ability.UIAbility'
 
-export default class TestAbility extends Ability {
+import UIAbility from "@ohos.app.ability.UIAbility";
+import AbilityDelegatorRegistry from "@ohos.app.ability.abilityDelegatorRegistry";
+import hilog from "@ohos.hilog";
+import { Hypium } from "@ohos/hypium";
+import testsuite from "../test/List.test";
+import window from "@ohos.window";
+import common from "@ohos.app.ability.common"
+export default class TestAbility extends UIAbility {
     onCreate(want, launchParam) {
-        console.log('TestAbility onCreate')
+        hilog.info(0x0000, "testTag", "%{public}s", "TestAbility onCreate");
+        hilog.info(0x0000, "testTag", "%{public}s", "want param:" + JSON.stringify(want) ?? "");
+        hilog.info(0x0000, "testTag", "%{public}s", "launchParam:" + JSON.stringify(launchParam) ?? "");
+        var abilityDelegator: any;
+        abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator();
+        var abilityDelegatorArguments: any;
+        abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments();
+        hilog.info(0x0000, "testTag", "%{public}s", "start run testcase!!!");
+        Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite);
+		AppStorage.SetOrCreate<common.UIAbilityContext>("TestAbilityContext",this.context)
     }
 
     onDestroy() {
-        console.log('TestAbility onDestroy')
+        hilog.info(0x0000, "testTag", "%{public}s", "TestAbility onDestroy");
     }
 
-    onWindowStageCreate(windowStage) {
-        console.log('TestAbility onWindowStageCreate')
-        windowStage.loadContent("TestAbility/pages/index", (err, data) => {
+    onWindowStageCreate(windowStage: window.WindowStage) {
+        hilog.info(0x0000, "testTag", "%{public}s", "TestAbility onWindowStageCreate");
+
+        windowStage.loadContent("TestAbility/pages/Index", (err, data) => {
             if (err.code) {
-                console.error('Failed to load the content. Cause:' + JSON.stringify(err));
+                hilog.error(0x0000, "testTag", "Failed to load the content. Cause: %{public}s", JSON.stringify(err) ?? "");
                 return;
             }
-            console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
+            hilog.info(0x0000, "testTag", "Succeeded in loading the content. Data: %{public}s", JSON.stringify(data) ?? "");
         });
-
-        globalThis.abilityContext = this.context;
     }
 
     onWindowStageDestroy() {
-        console.log('TestAbility onWindowStageDestroy')
+        hilog.info(0x0000, "testTag", "%{public}s", "TestAbility onWindowStageDestroy");
     }
 
     onForeground() {
-        console.log('TestAbility onForeground')
+        hilog.info(0x0000, "testTag", "%{public}s", "TestAbility onForeground");
     }
 
     onBackground() {
-        console.log('TestAbility onBackground')
+        hilog.info(0x0000, "testTag", "%{public}s", "TestAbility onBackground");
     }
-};
+}
