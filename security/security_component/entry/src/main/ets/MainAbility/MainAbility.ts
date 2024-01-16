0552d8688692a38476e9ada429f16d464c2ffa65
yzytank <zhongyu.yuan@i-soft.com.cn>
2024-01-16 18:15:36 +0800

    update
    Signed-off-by: yzytank <zhongyu.yuan@i-soft.com.cn>

--- security/security_component/entry/src/main/ets/MainAbility/MainAbility.ts
+++ security/security_component/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -4,37 +4,37 @@ import AbilityConstant from '@ohos.app.ability.AbilityConstant'
 import GlobalContext from '../test/GlobalContext'
 
 export default class MainAbility extends Ability {
-    onCreate(want: Want, launchParam: AbilityConstant.LaunchParam){
-        // Ability is creating, initialize resources for this ability
-        console.log("[Demo] MainAbility onCreate")
-        GlobalContext.getContext().setObject('abilityWant', want);
-    }
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    // Ability is creating, initialize resources for this ability
+    console.log("[Demo] MainAbility onCreate")
+    GlobalContext.getContext().setObject('abilityWant', want);
+  }
 
-    onDestroy() {
-        // Ability is destroying, release resources for this ability
-        console.log("[Demo] MainAbility onDestroy")
-    }
+  onDestroy() {
+    // Ability is destroying, release resources for this ability
+    console.log("[Demo] MainAbility onDestroy")
+  }
 
-    onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
-        console.log("[Demo] MainAbility onWindowStageCreate windowStage="+ windowStage)
-        GlobalContext.getContext().setObject('abilityContext', this.context);
-        GlobalContext.getContext().setObject('windowStage', windowStage);
-        windowStage.setUIContent(this.context, "MainAbility/pages/index/index", null)
-    }
+  onWindowStageCreate(windowStage) {
+    // Main window is created, set main page for this ability
+    console.log("[Demo] MainAbility onWindowStageCreate windowStage=" + windowStage)
+    GlobalContext.getContext().setObject('abilityContext', this.context);
+    GlobalContext.getContext().setObject('windowStage', windowStage);
+    windowStage.setUIContent(this.context, "MainAbility/pages/index/index", null)
+  }
 
-    onWindowStageDestroy() {
-        //Main window is destroyed, release UI related resources
-        console.log("[Demo] MainAbility onWindowStageDestroy")
-    }
+  onWindowStageDestroy() {
+    //Main window is destroyed, release UI related resources
+    console.log("[Demo] MainAbility onWindowStageDestroy")
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        console.log("[Demo] MainAbility onForeground")
-    }
+  onForeground() {
+    // Ability has brought to foreground
+    console.log("[Demo] MainAbility onForeground")
+  }
 
-    onBackground() {
-        // Ability has back to background
-        console.log("[Demo] MainAbility onBackground")
-    }
+  onBackground() {
+    // Ability has back to background
+    console.log("[Demo] MainAbility onBackground")
+  }
 };
