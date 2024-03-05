d4e9d1f514437335ebd1f7a7650440d352dbe72a
liangweitao <liangweitao@openvalley.net>
2024-03-05 15:34:48 +0800

    ActsCallFunctionTest/ActsCallRely
    Signed-off-by: liangweitao <liangweitao@openvalley.net>

--- ability/ability_runtime/actsmultiplecall/actsapprely/entry/src/main/ets/secondability/SecondAbility.ts
+++ ability/ability_runtime/actsmultiplecall/actsapprely/entry/src/main/ets/secondability/SecondAbility.ts
@@ -12,9 +12,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import hilog from '@ohos.hilog';
 import Ability from '@ohos.app.ability.UIAbility';
-import type Window from '@ohos.window';
+import Window from '@ohos.window';
 
 export default class SecondAbility extends Ability {
   onCreate(want, launchParam) {
