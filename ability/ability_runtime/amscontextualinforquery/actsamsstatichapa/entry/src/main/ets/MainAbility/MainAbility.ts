9c0c04cda26c0e36d80fad48411d2052d1d2a0c1
liangweitao <liangweitao@openvalley.net>
2024-03-15 17:07:12 +0800

    ActsAmsStaticInformationMultipleTest/ActsAmsStaticARelyHap
    Signed-off-by: liangweitao <liangweitao@openvalley.net>

--- ability/ability_runtime/amscontextualinforquery/actsamsstatichapa/entry/src/main/ets/MainAbility/MainAbility.ts
+++ ability/ability_runtime/amscontextualinforquery/actsamsstatichapa/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -12,72 +12,74 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Ability from '@ohos.app.ability.UIAbility'
-import commonEvent from '@ohos.commonEvent'
+
+import Ability from '@ohos.app.ability.UIAbility';
+import commonEvent from '@ohos.commonEvent';
+import Want from '@ohos.app.ability.Want';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+import window from '@ohos.window';
 
 export default class MainAbility extends Ability {
-    onCreate(want, launchParam) {
-        // Ability is creating, initialize resources for this ability
-        console.log("ACTS_getAbilityInfo_0400 MainAbility onCreate")
-        globalThis.abilityWant = want;
-    }
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    // Ability is creating, initialize resources for this ability
+    console.log("ACTS_getAbilityInfo_0400 MainAbility onCreate");
+    AppStorage.setOrCreate<Want>("abilityWant", want);
+  }
 
-    onDestroy() {
-        // Ability is destroying, release resources for this ability
-        console.log("ACTS_getAbilityInfo_0400 MainAbility onDestroy")
-    }
+  onDestroy() {
+    // Ability is destroying, release resources for this ability
+    console.log("ACTS_getAbilityInfo_0400 MainAbility onDestroy");
+  }
 
-    onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
-        console.log("ACTS_getAbilityInfo_0400 MainAbility onWindowStageCreate")
-        globalThis.abilityContext = this.context
-        windowStage.setUIContent(this.context, "pages/index", null)
-    }
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    console.log("ACTS_getAbilityInfo_0400 MainAbility onWindowStageCreate");
+    windowStage.loadContent("pages/index", null);
+  }
 
-    onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
-        console.log("ACTS_getAbilityInfo_0400 MainAbility onWindowStageDestroy")
-    }
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    console.log("ACTS_getAbilityInfo_0400 MainAbility onWindowStageDestroy");
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        console.log("ACTS_getAbilityInfo_0400 MainAbility onForeground")
-        if (globalThis.abilityWant.action == 'startHapA') {
-            globalThis.abilityContext.startAbility({
-                bundleName: "com.acts.stinformationmulapp",
-                abilityName: "com.example.feature.MainAbility",
-                action: "startHapB"
-            }, (error, data) => {
-                console.log('ACTS_getAbilityInfo_0400 MainAbility onForeground - startAbility restart singleton: '
-                + JSON.stringify(error) + ", " + JSON.stringify(data))
-            })
-            commonEvent.publish("MainAbility_Start_CommonEvent_multi_hap_entry", {
-                parameters: {
-                    hapModuleInfo: JSON.stringify(globalThis.abilityContext.currentHapModuleInfo),
-                    applicationInfo: JSON.stringify(globalThis.abilityContext.applicationInfo),
-                    abilityInfo: JSON.stringify(globalThis.abilityContext.abilityInfo)
-                }
-            }, (err) => {
-                console.log("ACTS_getAbilityInfo_0400 MainAbility Publish CallBack onForeground")
-            });
+  onForeground() {
+    // Ability has brought to foreground
+    console.log("ACTS_getAbilityInfo_0400 MainAbility onForeground");
+    if (AppStorage.get<Want>("abilityWant")!.action == 'startHapA') {
+      this.context.startAbility({
+        bundleName: "com.acts.stinformationmulapp",
+        abilityName: "com.example.feature.MainAbility",
+        action: "startHapB"
+      }, (error, data) => {
+        console.log('ACTS_getAbilityInfo_0400 MainAbility onForeground - startAbility restart singleton: ' + JSON.stringify(error) + ", " + JSON.stringify(data));
+      })
+      commonEvent.publish("MainAbility_Start_CommonEvent_multi_hap_entry", {
+        parameters: {
+          hapModuleInfo: JSON.stringify(this.context.currentHapModuleInfo),
+          applicationInfo: JSON.stringify(this.context.applicationInfo),
+          abilityInfo: JSON.stringify(this.context.abilityInfo)
         }
-        console.log("ACTS_getAbilityInfo_0400 MainAbility onForeground")
+      }, (err) => {
+        console.log("ACTS_getAbilityInfo_0400 MainAbility Publish CallBack onForeground");
+      });
+    }
+    console.log("ACTS_getAbilityInfo_0400 MainAbility onForeground");
 
-        if (globalThis.abilityWant.action == 'multiple') {
-            commonEvent.publish("MainAbility_Start_CommonEvent_multiple_hap_entry", {
-                parameters: {
-                    hapModuleInfo: JSON.stringify(globalThis.abilityContext.currentHapModuleInfo),
-                    applicationInfo: JSON.stringify(globalThis.abilityContext.applicationInfo),
-                    abilityInfo: JSON.stringify(globalThis.abilityContext.abilityInfo)
-                }
-            }, (err) => {
-                console.log("ACTS_getAbilityInfo_0400 MainAbility Publish CallBack onForeground")
-            });
+    if (AppStorage.get<Want>("abilityWant")!.action == 'multiple') {
+      commonEvent.publish("MainAbility_Start_CommonEvent_multiple_hap_entry", {
+        parameters: {
+          hapModuleInfo: JSON.stringify(this.context.currentHapModuleInfo),
+          applicationInfo: JSON.stringify(this.context.applicationInfo),
+          abilityInfo: JSON.stringify(this.context.abilityInfo)
         }
+      }, (err) => {
+        console.log("ACTS_getAbilityInfo_0400 MainAbility Publish CallBack onForeground");
+      });
     }
+  }
 
-    onBackground() {
-        // Ability has back to background
-        console.log("ACTS_getAbilityInfo_0400 MainAbility onBackground")
-    }
-};
+  onBackground() {
+    // Ability has back to background
+    console.log("ACTS_getAbilityInfo_0400 MainAbility onBackground");
+  }
+}
