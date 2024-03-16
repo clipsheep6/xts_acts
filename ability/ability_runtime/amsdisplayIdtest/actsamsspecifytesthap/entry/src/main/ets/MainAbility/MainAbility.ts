02910664a6160617c532cb2a11bf9c906fdbc93a
liangweitao <liangweitao@openvalley.net>
2024-03-16 14:30:37 +0800

    ActsAmsStartAbilityTest/ActsAmsSpecifyTestRelyHap
    Signed-off-by: liangweitao <liangweitao@openvalley.net>

--- ability/ability_runtime/amsdisplayIdtest/actsamsspecifytesthap/entry/src/main/ets/MainAbility/MainAbility.ts
+++ ability/ability_runtime/amsdisplayIdtest/actsamsspecifytesthap/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -12,109 +12,117 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import Ability from '@ohos.app.ability.UIAbility'
-import commonEvent from '@ohos.commonEvent'
 
-var callBackSeq = "";
+import Ability from '@ohos.app.ability.UIAbility';
+import commonEvent from '@ohos.commonEvent';
+import Want from '@ohos.app.ability.Want';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+import window from '@ohos.window';
+import common from '@ohos.app.ability.common';
+import App from '@system.app';
+
+let callBackSeq = "";
 
 function publishCallBack() {
-    console.debug("====>Publish CallBack ====>");
+  console.debug("====>Publish CallBack ====>");
 }
 
 async function startAbilityProcess(abilityContext, options, parameters) {
-    let bundleName;
-    let abilityName;
+  let bundleName;
+  let abilityName;
 
-    let idx = parameters.nextStep;
-    switch (parameters.step[idx]) {
-        case "testA":
-            bundleName = "com.example.actsspecifytesthap";
-            abilityName = "com.example.actsspecifytesthap.MainAbility";
-            break;
-        default:
-            break;
-    }
-    parameters.nextStep = ++idx;
-    abilityContext.startAbility({
-        bundleName: bundleName,
-        abilityName: abilityName,
-        parameters: parameters,
-    }, options, (error, data) => {
-        console.log('startAbilityProcess result: ' + JSON.stringify(error) + ", " + JSON.stringify(data))
-    })
+  let idx = parameters.nextStep;
+  switch (parameters.step[idx]) {
+    case "testA":
+      bundleName = "com.example.actsspecifytesthap";
+      abilityName = "com.example.actsspecifytesthap.MainAbility";
+      break;
+    default:
+      break;
+  }
+  parameters.nextStep = ++idx;
+  abilityContext.startAbility({
+    bundleName: bundleName,
+    abilityName: abilityName,
+    parameters: parameters,
+  }, options, (error, data) => {
+    console.log('startAbilityProcess result: ' + JSON.stringify(error) + ", " + JSON.stringify(data));
+  })
 }
 
 async function onShowProcess() {
-    var abilityWant = globalThis.abilityWant;
-    var callBackData = "actsspecifytesthap:"
-    callBackData += callBackSeq;
+  let abilityWant = AppStorage.get<Want>("abilityWant")!;
+  let callBackData = "actsspecifytesthap:";
+  callBackData += callBackSeq;
 
-    var commonEventPublishData = {
-        data: callBackData,
-        parameters: {
-            displayId: globalThis.abilityWant.parameters['ohos.aafwk.param.displayId'],
-            windowMode: globalThis.abilityWant.parameters['ohos.aafwk.param.windowMode'],
-}
-    };
-
-    console.log("AbilityMultiInstanceAppA abilityWant = " + JSON.stringify(abilityWant));
-    if (abilityWant.parameters.nextStep >= abilityWant.parameters.stepNum) {
-        commonEvent.publish("MultiInstanceStartFinish", commonEventPublishData, () => {
-            console.log('============>querytestsecond success==========>>')
-            callBackSeq = "";
-        });
-    } else {
-        commonEvent.publish("MultiInstanceStartNext", commonEventPublishData, () => {
-            callBackSeq = "";
-            console.log('============>querytest success==========>>')
-            startAbilityProcess(globalThis.abilityContext, globalThis.abilityWant.options, abilityWant.parameters);
-        });
+  let commonEventPublishData = {
+    data: callBackData,
+    parameters: {
+      displayId: abilityWant.parameters['ohos.aafwk.param.displayId'],
+      windowMode: abilityWant.parameters['ohos.aafwk.param.windowMode'],
     }
+  };
+
+  console.log("AbilityMultiInstanceAppA abilityWant = " + JSON.stringify(abilityWant));
+  if (abilityWant.parameters.nextStep >= abilityWant.parameters.stepNum) {
+    commonEvent.publish("MultiInstanceStartFinish", commonEventPublishData, () => {
+      console.log('============>querytestsecond success==========>>')
+      callBackSeq = "";
+    });
+  } else {
+    commonEvent.publish("MultiInstanceStartNext", commonEventPublishData, () => {
+      callBackSeq = "";
+      console.log('============>querytest success==========>>')
+      startAbilityProcess(AppStorage.get<common.UIAbilityContext>("abilityContext")!, undefined, abilityWant.parameters);
+    });
+  }
 }
 
 export default class MainAbility extends Ability {
-    onCreate(want, launchParam) {
-        // Ability is creating, initialize resources for this ability
-        console.log("actsspecifytesthap onCreate")
-        callBackSeq += "onCreate";
-        globalThis.abilityWant = want;
-    }
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    // Ability is creating, initialize resources for this ability
+    console.log("actsspecifytesthap onCreate");
+    callBackSeq += "onCreate";
+    AppStorage.setOrCreate<Want>("abilityWant", want);
+  }
 
-    onDestroy() {
-        // Ability is destroying, release resources for this ability
-        console.log("actsspecifytesthap onDestroy")
-    }
+  onDestroy() {
+    // Ability is destroying, release resources for this ability
+    console.log("actsspecifytesthap onDestroy");
+  }
 
-    onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
-        console.log("actsspecifytesthap onWindowStageCreate")
-        //       callBackSeq += "onWindowStageCreate";
-        globalThis.abilityContext = this.context
-        windowStage.setUIContent(this.context, "pages/index/index", null)
-        console.log("actsspecifytesthap onWindowStageCreate finish")
-    }
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    console.log("actsspecifytesthap onWindowStageCreate");
+    //       callBackSeq += "onWindowStageCreate";
+    AppStorage.setOrCreate<common.UIAbilityContext>("abilityContext", this.context);
+    windowStage.loadContent("pages/index/index", null);
+    console.log("actsspecifytesthap onWindowStageCreate finish");
+  }
 
-    onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
-        console.log("actsspecifytesthap onWindowStageDestroy")
-    }
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    console.log("actsspecifytesthap onWindowStageDestroy");
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        console.log("actsspecifytesthap onForeground")
-        callBackSeq += "onForeground";
-        onShowProcess();
-        setTimeout(()=>{
-          this.context.terminateSelf().then((data) => {
-              console.info("actsspecifytesthap MainAbility terminateSelf data = " + JSON.stringify(data));
-          }).catch((err) => {
-              console.info("actsspecifytesthap MainAbility terminateSelf err = " + JSON.stringify(err));
-          });
-      }, 2000)
-    }
+  onForeground() {
+    // Ability has brought to foreground
+    console.log("actsspecifytesthap onForeground");
+    callBackSeq += "onForeground";
+    onShowProcess();
+    setTimeout(() => {
+      this.context.terminateSelf()
+        .then((data) => {
+          console.info("actsspecifytesthap MainAbility terminateSelf data = " + JSON.stringify(data));
+        })
+        .catch((err) => {
+          console.info("actsspecifytesthap MainAbility terminateSelf err = " + JSON.stringify(err));
+        });
+    }, 2000)
+  }
 
-    onBackground() {
-        // Ability has back to background
-        console.log("actsspecifytesthap onBackground")
-    }
-};
+  onBackground() {
+    // Ability has back to background
+    console.log("actsspecifytesthap onBackground");
+  }
+}
