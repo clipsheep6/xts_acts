05b94a8131d12e42b341227987dfab077594a934
liangweitao <liangweitao@openvalley.net>
2024-03-16 11:29:43 +0800

    amsdisplayIdtest/ActsAmsStartAbilityForResultTest
    Signed-off-by: liangweitao <liangweitao@openvalley.net>

--- ability/ability_runtime/amsdisplayIdtest/actsamsstartabilityforresulttest/entry/src/main/ets/MainAbility2/MainAbility2.ts
+++ ability/ability_runtime/amsdisplayIdtest/actsamsstartabilityforresulttest/entry/src/main/ets/MainAbility2/MainAbility2.ts
@@ -12,61 +12,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Ability from '@ohos.app.ability.UIAbility'
-import commonEvent from '@ohos.commonEvent'
+
+import Ability from '@ohos.app.ability.UIAbility';
+import commonEvent from '@ohos.commonEvent';
+import Want from '@ohos.app.ability.Want';
+import common from '@ohos.app.ability.common';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+import window from '@ohos.window';
 
 async function onShowProcess() {
-    var abilityWant = globalThis.abilityWant2;
+  let abilityWant = AppStorage.get<Want>("abilityWant2")!;
 
-    var commonEventPublishData = {
-        parameters: {
-            displayId: abilityWant.parameters['ohos.aafwk.param.displayId'],
-            windowMode: abilityWant.parameters['ohos.aafwk.param.windowMode'],
-        }
-    };
-   
-    console.log("AbilityMultiInstanceAppA abilityWant = " + JSON.stringify(abilityWant));
-  
-    commonEvent.publish("ACTS_TerminateSelf_CommonEvent", commonEventPublishData, () => {
-        console.log('============>querytestsecond success==========>>')
-        globalThis.abilityContext2.terminateSelf();
-    });
-    
+  let commonEventPublishData = {
+    parameters: {
+      displayId: abilityWant.parameters['ohos.aafwk.param.displayId'],
+      windowMode: abilityWant.parameters['ohos.aafwk.param.windowMode'],
+    }
+  };
+  console.log("AbilityMultiInstanceAppA abilityWant = " + JSON.stringify(abilityWant));
+  commonEvent.publish("ACTS_TerminateSelf_CommonEvent", commonEventPublishData, () => {
+    console.log('============>querytestsecond success==========>>');
+    AppStorage.get<common.UIAbilityContext>("abilityContext2")!.terminateSelf();
+  });
+
 }
+
 export default class MainAbility extends Ability {
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    // Ability is creating, initialize resources for this ability
+    console.log("MainAbility2 onCreate")
+    AppStorage.setOrCreate<Want>("abilityWant2", want);
+    console.log("AbilityMultiInstanceAppA abilityWant = " + JSON.stringify(want));
+  }
 
-    onCreate(want, launchParam) {
-        // Ability is creating, initialize resources for this ability
-        console.log("MainAbility2 onCreate")
-        globalThis.abilityWant2 = want;
-        console.log("AbilityMultiInstanceAppA abilityWant = " + JSON.stringify( globalThis.abilityWant2));
-    }
+  onDestroy() {
+    // Ability is destroying, release resources for this ability
+    console.log("MainAbility2 onDestroy");
+  }
 
-    onDestroy() {
-        // Ability is destroying, release resources for this ability
-        console.log("MainAbility2 onDestroy")
-    }
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    console.log("MainAbility2 onWindowStageCreate");
+    AppStorage.setOrCreate<common.UIAbilityContext>("abilityContext2", this.context);
+    windowStage.loadContent("MainAbility/pages/second/second", null);
+  }
 
-    onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
-        console.log("MainAbility2 onWindowStageCreate")
-        globalThis.abilityContext2 = this.context
-        windowStage.setUIContent(this.context, "MainAbility/pages/second/second", null)
-    }
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    console.log("MainAbility onWindowStageDestroy");
+  }
 
-    onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
-        console.log("MainAbility onWindowStageDestroy")
-    }
+  onForeground() {
+    // Ability has brought to foreground
+    console.log("MainAbility onForeground");
+    onShowProcess();
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        console.log("MainAbility onForeground")
-        onShowProcess();
-    }
-
-    onBackground() {
-        // Ability has back to background
-        console.log("MainAbility onBackground")
-    }
-};
+  onBackground() {
+    // Ability has back to background
+    console.log("MainAbility onBackground");
+  }
+}
