6995b585c004a010ceb50cf6d948c6d0f9b29fdc
liangweitao <liangweitao@openvalley.net>
2024-03-23 15:09:14 +0800

    ActsTempUriPermissionHapTest/ActsAssistantHap
    Signed-off-by: liangweitao <liangweitao@openvalley.net>

--- ability/ability_runtime/stage/actsuripermission/actsassistanthap/entry/src/main/ets/Hap2MainAbility2/Hap2MainAbility2.ts
+++ ability/ability_runtime/stage/actsuripermission/actsassistanthap/entry/src/main/ets/Hap2MainAbility2/Hap2MainAbility2.ts
@@ -13,86 +13,93 @@
  * limitations under the License.
  */
 
-import Ability from '@ohos.app.ability.UIAbility'
+import Ability from '@ohos.app.ability.UIAbility';
 import commonEvent from '@ohos.commonEvent';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+import Want from '@ohos.app.ability.Want';
+import window from '@ohos.window';
 
 export default class Hap2MainAbility2 extends Ability {
-    onCreate(want, launchParam) {
-        console.log('[Demo] Hap2MainAbility2 onCreate')
-        globalThis.abilityWant2 = want;
-    }
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    console.log('[Demo] Hap2MainAbility2 onCreate');
+    globalThis.abilityWant2 = want;
+  }
 
-    onDestroy() {
-        console.log('[Demo] Hap2MainAbility2 onDestroy')
-    }
+  onDestroy() {
+    console.log('[Demo] Hap2MainAbility2 onDestroy');
+  }
 
-    onWindowStageCreate(windowStage) {
-        console.log('[Demo] Hap2MainAbility2 onWindowStageCreate')
-        windowStage.setUIContent(this.context, 'pages/Hap2MainAbility2_pages', null)
-    }
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    console.log('[Demo] Hap2MainAbility2 onWindowStageCreate');
+    windowStage.loadContent('pages/Hap2MainAbility2_pages', null);
+  }
 
-    onWindowStageDestroy() {
-        console.log('[Demo] Hap2MainAbility2 onWindowStageDestroy')
-    }
+  onWindowStageDestroy() {
+    console.log('[Demo] Hap2MainAbility2 onWindowStageDestroy');
+  }
 
-    onNewWant(want, launchParams) {
-        globalThis.abilityWant2 = want;
-    }
+  onNewWant(want, launchParams) {
+    globalThis.abilityWant2 = want;
+  }
 
-    onForeground() {
-        console.log('[Demo] Hap2MainAbility2 onForeground')
-        switch (globalThis.abilityWant2.parameters['number']) {
-            case 20: {
-                let uri : string = globalThis.abilityWant2.uri;
-                if (uri == null) {
-                    console.log('[Demo] Hap2MainAbility2 uri is invalid');
-                    return;
-                }
-                let eventName20_1 : string = 'Temp_UriPermissionTest_0200_Read_Successfully';
-                let eventName20_2 : string = 'Temp_UriPermissionTest_0200_Read_Failed';
-                globalThis.fs.open(uri, globalThis.fs.OpenMode.READ_ONLY).then((file) => {
-                    console.info('read file fd: ' + file.fd);
-                    commonEvent.publish(eventName20_1, (err) => {
-                        console.log('Hap2MainAbility2' + eventName20_1);
-                    });
-                }).catch((err) => {
-                    console.info('read open file failed with error message: ' + err.message + ', error code: ' + err.code);
-                    commonEvent.publish(eventName20_2, (err) => {
-                        console.log('Hap2MainAbility2' + eventName20_2);
-                    });
-                });
-
-                let eventName20_3 : string = 'Temp_UriPermissionTest_0200_Write_Successfully';
-                let eventName20_4 : string = 'Temp_UriPermissionTest_0200_Write_Failed';
-                globalThis.fs.open(uri, globalThis.fs.OpenMode.READ_WRITE).then((file) => {
-                    console.info('write file fd: ' + file.fd);
-                    commonEvent.publish(eventName20_3, (err) => {
-                        console.log('Hap2MainAbility2' + eventName20_3);
-                    });
-                }).catch((err) => {
-                    console.info('write open file failed with error message: ' + err.message + ', error code: ' + err.code);
-                    commonEvent.publish(eventName20_4, (err) => {
-                        console.log('Hap2MainAbility2' + eventName20_4);
-                    });
-                });
-                break;
-            }
-
-            case 21: {
-                let eventName : string = 'StartDone_Hap2MainAbility2';
-                commonEvent.publish(eventName, (err) => {
-                    console.log('Hap2MainAbility2' + eventName);
-                });
-                break;
-            }
-
-            default:
-                break;
+  onForeground() {
+    console.log('[Demo] Hap2MainAbility2 onForeground');
+    switch (globalThis.abilityWant2.parameters['number']) {
+      case 20: {
+        let uri: string = globalThis.abilityWant2.uri;
+        if (uri == null) {
+          console.log('[Demo] Hap2MainAbility2 uri is invalid');
+          return;
         }
-        
+        let eventName20_1: string = 'Temp_UriPermissionTest_0200_Read_Successfully';
+        let eventName20_2: string = 'Temp_UriPermissionTest_0200_Read_Failed';
+        globalThis.fs.open(uri, globalThis.fs.OpenMode.READ_ONLY)
+          .then((file) => {
+            console.info('read file fd: ' + file.fd);
+            commonEvent.publish(eventName20_1, (err) => {
+              console.log('Hap2MainAbility2' + eventName20_1);
+            });
+          })
+          .catch((err) => {
+            console.info('read open file failed with error message: ' + err.message + ', error code: ' + err.code);
+            commonEvent.publish(eventName20_2, (err) => {
+              console.log('Hap2MainAbility2' + eventName20_2);
+            });
+          });
+
+        let eventName20_3: string = 'Temp_UriPermissionTest_0200_Write_Successfully';
+        let eventName20_4: string = 'Temp_UriPermissionTest_0200_Write_Failed';
+        globalThis.fs.open(uri, globalThis.fs.OpenMode.READ_WRITE)
+          .then((file) => {
+            console.info('write file fd: ' + file.fd);
+            commonEvent.publish(eventName20_3, (err) => {
+              console.log('Hap2MainAbility2' + eventName20_3);
+            });
+          })
+          .catch((err) => {
+            console.info('write open file failed with error message: ' + err.message + ', error code: ' + err.code);
+            commonEvent.publish(eventName20_4, (err) => {
+              console.log('Hap2MainAbility2' + eventName20_4);
+            });
+          });
+        break;
+      }
+
+      case 21: {
+        let eventName: string = 'StartDone_Hap2MainAbility2';
+        commonEvent.publish(eventName, (err) => {
+          console.log('Hap2MainAbility2' + eventName);
+        });
+        break;
+      }
+
+      default:
+        break;
     }
 
-    onBackground() {
-        console.log('[Demo] Hap2MainAbility2 onBackground')
-    }
+  }
+
+  onBackground() {
+    console.log('[Demo] Hap2MainAbility2 onBackground');
+  }
 };
