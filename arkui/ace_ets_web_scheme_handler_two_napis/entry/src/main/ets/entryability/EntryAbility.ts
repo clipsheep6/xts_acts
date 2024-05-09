82c2c0fced755cccd8d6761cfd0ad57d41de8b4b
h30051541 <2219695650@qq.com>
2024-05-09 23:17:37 +0800

    xts_tijiao_two
    Signed-off-by: h30051541 <2219695650@qq.com>

---
+++ arkui/ace_ets_web_scheme_handler_two_napis/entry/src/main/ets/entryability/EntryAbility.ts
@@ -0,0 +1,49 @@
+import UIAbility from '@ohos.app.ability.UIAbility';
+import hilog from '@ohos.hilog';
+import window from '@ohos.window';
+import testNapi from 'libentry.so'
+import webview from '@ohos.web.webview';
+
+export default class EntryAbility extends UIAbility {
+  onCreate(want, launchParam) {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    //注册三方 协议的配置
+    testNapi.registerCustomSchemes();
+    //初始化web组件内核，该操作会初始化Browser进程以及创建BrowserContext
+    webview.WebviewController.initializeWebEngine();
+    //设置SchemeHandler
+    testNapi.setSchemeHandler();
+  }
+
+  onDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
+  }
+
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+    windowStage.loadContent('pages/Index', (err, data) => {
+      if (err.code) {
+        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        return;
+      }
+      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    });
+  }
+
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
+  }
+
+  onForeground() {
+    // Ability has brought to foreground
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
+  }
+
+  onBackground() {
+    // Ability has back to background
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+  }
+};
