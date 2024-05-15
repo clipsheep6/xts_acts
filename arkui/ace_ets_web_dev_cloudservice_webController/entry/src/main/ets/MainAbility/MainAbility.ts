d800744b93da3aad95022edfd372623739330675
Bingqing Wu <wubingqing6@huawei.com>
2024-05-15 16:29:25 +0800

    change agian
    Signed-off-by: Bingqing Wu <wubingqing6@huawei.com>

--- arkui/ace_ets_web_dev_cloudservice_webController/entry/src/main/ets/MainAbility/MainAbility.ts
+++ arkui/ace_ets_web_dev_cloudservice_webController/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -31,7 +31,7 @@ export default class MainAbility extends Ability {
     console.log("[Demo] MainAbility onWindowStageCreate windowStage=" + windowStage);
     globalThis.windowStage = windowStage;
     globalThis.abilityContext = this.context;
-    windowStage.setUIContent(this.context, "MainAbility/pages/webOnBeforeUnload", null);
+    windowStage.setUIContent(this.context, "MainAbility/pages/webViewGetRenderProcessMode", null);
   }
 
   onWindowStageDestroy() {
