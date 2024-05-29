792bd9912b9ce29fcb9a1df2fadfa6eb11b027cf
张建 <8725864+zhang-jian008@user.noreply.gitee.com>
2024-05-29 01:28:35 +0000

    update graphic/graphicEGL/entry/src/main/cpp/eglndk.cpp.
    Signed-off-by: 张建 <8725864+zhang-jian008@user.noreply.gitee.com>

--- graphic/graphicEGL/entry/src/main/cpp/eglndk.cpp
+++ graphic/graphicEGL/entry/src/main/cpp/eglndk.cpp
@@ -374,10 +374,6 @@ static napi_value EglGetConfigAttrib(napi_env env, napi_callback_info info)
         Ret = eglGetConfigAttrib(m_eglDisplay, m_eglConf[i], EGL_BUFFER_SIZE, &numConfigs);
     }
     NAPI_ASSERT(env, Ret == TRUE, "eglGetConfigAttrib error");
-    for (int i = 0; i < numConfigs; ++i) {
-        Ret = eglGetConfigAttrib(m_eglDisplay, m_eglConf[i], EGL_ALPHA_SIZE, &numConfigs);
-    }
-    NAPI_ASSERT(env, Ret == TRUE, "eglGetConfigAttrib error");
     eglReleaseThread();
     eglTerminate(m_eglDisplay);
     napi_value result = nullptr;
@@ -395,10 +391,6 @@ static napi_value EglGetConfigAttribAbnormal(napi_env env, napi_callback_info in
     EGLBoolean Ret;
     for (int i = 0; i < numConfigs; ++i) {
         Ret = eglGetConfigAttrib(m_eglDisplay, m_eglConf[i], EGL_BUFFER_SIZE, &numConfigs);
-    }
-    NAPI_ASSERT(env, Ret == FALSE, "eglGetConfigAttrib error");
-    for (int i = 0; i < numConfigs; ++i) {
-        Ret = eglGetConfigAttrib(m_eglDisplay, m_eglConf[i], EGL_ALPHA_SIZE, &numConfigs);
     }
     NAPI_ASSERT(env, Ret == FALSE, "eglGetConfigAttrib error");
     eglReleaseThread();
