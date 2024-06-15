3e42c65b9989f02d592d6367a165dd0963091c82
chenkang <chenkang85@huawei.com>
2024-06-15 15:50:51 +0800

    incrase batterylist okwap
    Signed-off-by: chenkang <chenkang85@huawei.com>

--- powermgr/battery_manager/entry/src/ohosTest/js/test/List.test.js
+++ powermgr/battery_manager/entry/src/ohosTest/js/test/List.test.js
@@ -11,6 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * okwap lookup
  */
 import batteryCommonEventTest from './battery_common_event.test.js'
 import batteryInfoTest from './batteryInfo.test.js'
