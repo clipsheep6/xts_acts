5cbf23aba74a4b3ef67bbcc0f126e867210c9d5d
mazongzhi <mazongzhi@huawei.com>
2024-06-21 17:32:19 +0900

    Picker view化需求XTS覆盖
    Signed-off-by: mazongzhi <mazongzhi@huawei.com>

---
+++ arkui/ace_ets_module_ui/ace_ets_module_textPicker/ace_ets_module_picker_api12/entry/src/main/ets/TestAbility/common/PhotoPickerResult.ts
@@ -0,0 +1,15 @@
+export class PhotoPickerResult{
+    checkBoxColor:string='';
+    backgroundColor:string ='';
+    isRepeatSelectSupported:boolean=false;
+    photoBrowserBackgroundColorMode:number=0;
+    maxSelectedReminderMode:number=0;
+    orientation:number=0;
+    themeColorMode:number=0;
+    selectMode:number=0;
+    maxSelectCount:number=500;
+    maxPhotoSelectNumber:number=500;
+    maxVideoSelectNumber:number=500;
+    itemType:string;
+    checkboxTextColor:string ='';
+}
