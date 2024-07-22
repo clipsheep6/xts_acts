ce5ea2a4820ac4aa030c1830f0935e95631136ee
陈广 <chenguang65@h-partners.com>
2024-07-22 14:27:54 +0000

    update AVSessionManager.test.js. update AVSessionManager.test.js.
    Signed-off-by: 陈广 <chenguang65@h-partners.com>

--- multimedia/avsession/avsession_js_standard/avsessionManager/entry/src/ohosTest/js/test/AVSessionManager.test.js
+++ multimedia/avsession/avsession_js_standard/avsessionManager/entry/src/ohosTest/js/test/AVSessionManager.test.js
@@ -38,14 +38,8 @@ export default function AVSessionManager() {
         })
 
         afterEach(async function () {
-            await sleep(5000);
             if (currentAVSession){
-                await currentAVSession.destroy().then(() => {
-                    console.info('TestLog: Session destroy success');
-                }).catch((err) => {
-                    console.info(`TestLog: Session destroy error: code: ${err.code}, message: ${err.message}`);
-                    expect(false).assertTrue();
-                });
+                await currentAVSession.destroy();
             }
             console.info('TestLog: End testing testcase');
         })
