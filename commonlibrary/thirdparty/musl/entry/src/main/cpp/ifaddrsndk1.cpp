80a631247d59f69148ef4fc9939e62249d544d52
yedexi <yedexi1@huawei.com>
2024-09-03 09:35:21 +0000

    update commonlibrary/thirdparty/musl/entry/src/main/cpp/ifaddrsndk1.cpp.
    Signed-off-by: yedexi <yedexi1@huawei.com>

--- commonlibrary/thirdparty/musl/entry/src/main/cpp/ifaddrsndk1.cpp
+++ commonlibrary/thirdparty/musl/entry/src/main/cpp/ifaddrsndk1.cpp
@@ -29,6 +29,7 @@ static napi_value Freeifaddrs(napi_env env, napi_callback_info info)
     if (!ret) {
         freeifaddrs(ifc);
     }
+    //This interface is under security control, and the permission may be insufficient on some devices.
     if (ret == FAIL && errno == EACCES) {
         ret = NO_ERR;
     }
