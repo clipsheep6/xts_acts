058290b837f83dae01f73670c51d12d632979df4
lWX1130500 <lidanyang16@huawei.com>
2024-09-18 11:21:52 +0800

    mod
    Signed-off-by: lWX1130500 <lidanyang16@huawei.com>

---
+++ sensors/miscdevice_ndk/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -0,0 +1,5 @@
+export const startVibrator: (duration: number, attribute: Object) => number;
+export const startVibratorCustom: (fd:number, offset:number, length:number, usage: number) => number;
+export const cancel: () => number;
+
+
