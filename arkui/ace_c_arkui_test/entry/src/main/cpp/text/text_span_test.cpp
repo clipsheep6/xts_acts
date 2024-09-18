cb794f30d42f55b5bb378f1f617e0bc320676f77
王广龙 <wangguanglong2@h-partners.com>
2024-09-18 14:56:59 +0800

    新增XComponent的xts覆盖
    Signed-off-by: 王广龙 <wangguanglong2@h-partners.com>

---
+++ arkui/ace_c_arkui_test/entry/src/main/cpp/text/text_span_test.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "common/common.h"
+
+namespace ArkUICapiTest {
+
+static napi_value testTextSpanContent001(napi_env env, napi_callback_info info)
+{
+    NAPI_START(text, ARKUI_NODE_TEXT);
+    NAPI_START(span, ARKUI_NODE_SPAN);
+    nodeAPI->addChild(text, span);
+    float sizeValue = 20;
+    ArkUI_NumberValue value[] = {{.f32 = sizeValue}, {.i32 = ARKUI_FONT_WEIGHT_W100}, {.i32 = ARKUI_FONT_STYLE_NORMAL}};
+    ArkUI_AttributeItem valueItem = {value, sizeof(value) / sizeof(ArkUI_NumberValue)};
+    valueItem.string = "textContent";
+    ArkUI_AttributeItem spanContent = {.string = "SpanContent"};
+    nodeAPI->setAttribute(text, NODE_TEXT_FONT, &valueItem);
+    auto ret = nodeAPI->setAttribute(span, NODE_SPAN_CONTENT, &spanContent);
+    ASSERT_EQ(ret, SUCCESS);
+    if (nodeAPI->getAttribute(text, NODE_TEXT_FONT)->string == nullptr) {
+        ASSERT_STREQ("", "SpanContent");
+    } else {
+        ASSERT_STREQ(nodeAPI->getAttribute(text, NODE_TEXT_FONT)->string, "SpanContent");
+    }
+    NAPI_END;
+}
+
+static napi_value testTextSpanFontSize002(napi_env env, napi_callback_info info)
+{
+    NAPI_START(text, ARKUI_NODE_TEXT);
+    NAPI_START(span, ARKUI_NODE_SPAN);
+    nodeAPI->addChild(text, span);
+    float size = -10;
+    float size1 = 16;
+    ArkUI_NumberValue value[] = {{.f32 = size}};
+    ArkUI_AttributeItem valueItem = {value, sizeof(value) / sizeof(ArkUI_NumberValue)};
+    auto ret = nodeAPI->setAttribute(text, NODE_FONT_SIZE, &valueItem);
+    ASSERT_EQ(ret, 401);
+    ASSERT_EQ(nodeAPI->getAttribute(text, NODE_FONT_SIZE)->value[PARAM_0].f32, size1);
+    NAPI_END;
+}
+
+static napi_value testTextSpanLineHeight003(napi_env env, napi_callback_info info)
+{
+    NAPI_START(text, ARKUI_NODE_TEXT);
+    NAPI_START(span, ARKUI_NODE_SPAN);
+    nodeAPI->addChild(text, span);
+    float height = -10;
+    ArkUI_NumberValue value[] = {{.f32 = height}};
+    ArkUI_AttributeItem valueItem = {value, sizeof(value) / sizeof(ArkUI_NumberValue)};
+    auto ret = nodeAPI->setAttribute(text, NODE_TEXT_LINE_HEIGHT, &valueItem);
+    ASSERT_EQ(ret, 401);
+    if (nodeAPI->getAttribute(text, NODE_TEXT_LINE_HEIGHT)->value[PARAM_0].f32 > 0) {
+        ASSERT_STREQ("1", "1");
+    } else {
+        ASSERT_STREQ("1", "2");
+    }
+    NAPI_END;
+}
+
+} // namespace ArkUICapiTest
