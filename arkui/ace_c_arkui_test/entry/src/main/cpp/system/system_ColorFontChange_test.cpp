16245a2fd3f36440a960d8b47e4066c515e03a16
刘从来 <liuconglai@h-partners.com>
2024-09-20 11:01:01 +0000

    update arkui/ace_c_arkui_test/entry/src/main/cpp/system/system_ColorFontChange_test.cpp.
    Signed-off-by: 刘从来 <liuconglai@h-partners.com>

--- arkui/ace_c_arkui_test/entry/src/main/cpp/system/system_ColorFontChange_test.cpp
+++ arkui/ace_c_arkui_test/entry/src/main/cpp/system/system_ColorFontChange_test.cpp
@@ -250,6 +250,7 @@ static napi_value testSystemFontStyleEvent_GetFontSizeScale_005(napi_env env, na
 
 static napi_value testSystemFontStyleEvent_GetFontWeightScale_006(napi_env env, napi_callback_info info)
 {
+    static napi_env env1 = env;
     ArkUI_NativeNodeAPI_1 *nodeAPI = nullptr;
     OH_ArkUI_GetModuleInterface(ARKUI_NATIVE_NODE, ArkUI_NativeNodeAPI_1, nodeAPI);
 
@@ -277,10 +278,11 @@ static napi_value testSystemFontStyleEvent_GetFontWeightScale_006(napi_env env, 
     
     auto onFontChange = [](ArkUI_SystemFontStyleEvent *fontStyle, void *userData)->void {
         OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "SystemFontStyleEvenTest", "OnEventReceive");
-        NAPI_ASSERT(env, fontStyle != null, "Failed to produce error condition");
-        
+        if (fontStyle == nullptr) {
+            napi_throw_error((env1), nullptr,
+                "assertion ( fontStyle != nullptr ) failed: Failed to produce error condition"); 
+        }
         auto fontWeight = OH_ArkUI_SystemFontStyleEvent_GetFontWeightScale(fontStyle);
-        
         OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_PRINT_DOMAIN, "Manager",
             "kkk onFontChange fontWeight = %{public}f", fontWeight);
     };
