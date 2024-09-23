d932cc593451987da7b5dc544125deb83fe91fa1
zhoubiao <zhoubiao20@huawei.com>
2024-09-23 21:41:29 +0800

    add pasteboard
    Signed-off-by: zhoubiao <zhoubiao20@huawei.com>

--- distributeddatamgr/Pasteboardnapitest/entry/src/main/cpp/napi_init.cpp
+++ distributeddatamgr/Pasteboardnapitest/entry/src/main/cpp/napi_init.cpp
@@ -9,7 +9,7 @@
 #include "database/udmf/udmf_meta.h"
 #include "database/udmf/uds.h"
 #include "database/udmf/utd.h"
-//#include "pasteboard_hilog.h"
+
 
 
 #include <iostream>
@@ -79,8 +79,6 @@ void* GetDataCallback(void* context, const char* type)
     return nullptr;
 }
 
-
-
 static napi_value OH_PasteboardSubscriber_Create0100(napi_env env, napi_callback_info info)
 {
     OH_PasteboardObserver* observer = OH_PasteboardObserver_Create();
@@ -90,7 +88,6 @@ static napi_value OH_PasteboardSubscriber_Create0100(napi_env env, napi_callback
     OH_PasteboardObserver_Destroy(nullptr);
     return result;
 }
-
 
 static napi_value OH_PasteboardObserver_SetData0100(napi_env env, napi_callback_info info)
 {
@@ -104,7 +101,6 @@ static napi_value OH_PasteboardObserver_SetData0100(napi_env env, napi_callback_
     OH_Pasteboard_Destroy(pasteboard);
     return returncode;
 }
-
 
 static napi_value OH_PasteboardObserver_SetData0200(napi_env env, napi_callback_info info)
 {
@@ -120,7 +116,6 @@ static napi_value OH_PasteboardObserver_SetData0200(napi_env env, napi_callback_
     OH_PasteboardObserver_Destroy(observer);
     OH_Pasteboard_Destroy(pasteboard);
     return result;
-
 }
 
 static napi_value OH_PasteboardObserver_SetData0300(napi_env env, napi_callback_info info)
@@ -136,7 +131,6 @@ static napi_value OH_PasteboardObserver_SetData0300(napi_env env, napi_callback_
     return result;
 }
 
-
 static napi_value OH_PasteboardObserver_SetData0400(napi_env env, napi_callback_info info)
 {
     napi_value result = nullptr;
@@ -148,10 +142,7 @@ static napi_value OH_PasteboardObserver_SetData0400(napi_env env, napi_callback_
     OH_PasteboardObserver_Destroy(observer);
     OH_Pasteboard_Destroy(pasteboard);
     return result;
-
 }
-
-
 
 static napi_value OH_Pasteboard_Create0100(napi_env env, napi_callback_info info)
 {
@@ -161,8 +152,6 @@ static napi_value OH_Pasteboard_Create0100(napi_env env, napi_callback_info info
     OH_Pasteboard_Destroy(pasteboard);
     return result;
 }
-
-
 
 static napi_value OH_Pasteboard_Subscribe0100(napi_env env, napi_callback_info info)
 {
@@ -175,9 +164,7 @@ static napi_value OH_Pasteboard_Subscribe0100(napi_env env, napi_callback_info i
     OH_Pasteboard_Destroy(pasteboard);
     OH_PasteboardObserver_Destroy(observer);
     return result;
-
 }
-
 
 static napi_value OH_Pasteboard_Subscribe0200(napi_env env, napi_callback_info info)
 {
@@ -191,7 +178,6 @@ static napi_value OH_Pasteboard_Subscribe0200(napi_env env, napi_callback_info i
     OH_PasteboardObserver_Destroy(observer);
     return result;
 }
-
 
 static napi_value OH_Pasteboard_Subscribe0300(napi_env env, napi_callback_info info)
 {
@@ -235,10 +221,7 @@ static napi_value OH_Pasteboard_Subscribe0500(napi_env env, napi_callback_info i
     OH_PasteboardObserver_Destroy(observer);
 
     return result;
-
 }
-
-
 
 static napi_value OH_Pasteboard_Subscribe0600(napi_env env, napi_callback_info info)
 
@@ -270,8 +253,6 @@ static napi_value OH_Pasteboard_Unsubcribe0100(napi_env env, napi_callback_info 
     OH_PasteboardObserver_Destroy(observer);
     return result;
 }
-
-
 
 static napi_value OH_Pasteboard_Unsubcribe0200(napi_env env, napi_callback_info info)
 {
@@ -316,9 +297,6 @@ static napi_value OH_Pasteboard_Unsubcribe0400(napi_env env, napi_callback_info 
     return result;
 }
 
-
-    
-
 static napi_value OH_Pasteboard_Unsubcribe0500(napi_env env, napi_callback_info info)
 {
     napi_value result = nullptr;
@@ -333,7 +311,6 @@ static napi_value OH_Pasteboard_Unsubcribe0500(napi_env env, napi_callback_info 
     return result;
 }
 
-
 static napi_value OH_Pasteboard_IsRemoteData0100(napi_env env, napi_callback_info info)
 {
     napi_value result = nullptr;
@@ -345,8 +322,6 @@ static napi_value OH_Pasteboard_IsRemoteData0100(napi_env env, napi_callback_inf
     OH_Pasteboard_Destroy(pasteboard);
     return result;
 }
-
-
 
 static napi_value OH_Pasteboard_GetDataSrouce0100(napi_env env, napi_callback_info info)
 {
@@ -369,9 +344,6 @@ static napi_value OH_Pasteboard_GetDataSrouce0100(napi_env env, napi_callback_in
     OH_Pasteboard_Destroy(pasteboard);
     return result;
 }
-
-
-
 
 static napi_value OH_Pasteboard_HasType0100(napi_env env, napi_callback_info info)
 {
@@ -454,8 +426,6 @@ static napi_value OH_Pasteboard_SetData0100(napi_env env, napi_callback_info inf
 }
 
 
-
-
 static napi_value OH_Pasteboard_GetData0100(napi_env env, napi_callback_info info)
 {
     napi_value result = nullptr;
@@ -483,8 +453,7 @@ static napi_value OH_Pasteboard_GetData0100(napi_env env, napi_callback_info inf
     OH_UdsPlainText *getPlainText = OH_UdsPlainText_Create();
     OH_UdmfRecord_GetPlainText(getRecords[0], getPlainText);
     const char *getContent = OH_UdsPlainText_GetContent(getPlainText);
-    //PASTEBOARD_HILOGI(PASTEBOARD_MODULE_SERVICE, "content is %{public}s", getContent);
-    
+ 
     NAPI_ASSERT(env, strcmp(getContent,content) == 0, "OH_Pasteboard_GetData is fail.");
     napi_create_int32(env, res == ERR_OK, &result);
     OH_Pasteboard_Destroy(pasteboard);
@@ -499,6 +468,7 @@ static napi_value OH_Pasteboard_GetData0100(napi_env env, napi_callback_info inf
 
 static napi_value OH_Pasteboard_GetData0200(napi_env env, napi_callback_info info)
 {
+    int num = 3;
     napi_value result = nullptr;
     OH_Pasteboard* pasteboard = OH_Pasteboard_Create();
     OH_UdmfData* setData = OH_UdmfData_Create();
@@ -509,7 +479,7 @@ static napi_value OH_Pasteboard_GetData0200(napi_env env, napi_callback_info inf
     OH_UdmfRecordProvider_SetData(provider, static_cast<void *>(record), GetDataCallback, ContextFinalizeFunc);
     OH_UdmfData_AddRecord(setData, record);
     const char* types[3] = { "general.plain-text", "general.hyperlink", "general.html" };
-    OH_UdmfRecord_SetProvider(record, types, 3, provider);
+    OH_UdmfRecord_SetProvider(record, types, num, provider);
     int res = OH_Pasteboard_SetData(pasteboard, setData);
     NAPI_ASSERT(env, res == ERR_OK, "OH_Pasteboard_SetData is fail.");
 
@@ -527,7 +497,6 @@ static napi_value OH_Pasteboard_GetData0200(napi_env env, napi_callback_info inf
     const char *getContent = OH_UdsPlainText_GetContent(getPlainText);
     NAPI_ASSERT(env, strcmp(getContent, PLAINTEXT_CONTENT) == 0, "OH_UdsPlainText_GetContent is fail.");
 
-
     OH_UdsHyperlink *getHyperLink = OH_UdsHyperlink_Create();
     OH_UdmfRecord_GetHyperlink(getRecords[0], getHyperLink);
     const char *getUrl = OH_UdsHyperlink_GetUrl(getHyperLink);
@@ -541,8 +510,6 @@ static napi_value OH_Pasteboard_GetData0200(napi_env env, napi_callback_info inf
     OH_UdmfData_Destroy(getData);
     return result;
 }
-
-
 
 static napi_value OH_Pasteboard_GetData0300(napi_env env, napi_callback_info info)
 {
@@ -562,19 +529,19 @@ static napi_value OH_Pasteboard_GetData0300(napi_env env, napi_callback_info inf
 
     int status = -1;
     OH_UdmfData* getData = OH_Pasteboard_GetData(pasteboard, &status);
-    NAPI_ASSERT(env,status == ERR_OK,"OH_GetData is fail");
-    NAPI_ASSERT(env,getData != nullptr,"OH_GetData is fail");
+    NAPI_ASSERT(env, status == ERR_OK, "OH_GetData is fail");
+    NAPI_ASSERT(env, getData != nullptr, "OH_GetData is fail");
 
     unsigned int getrecordCount = 0;
     OH_UdmfRecord **getRecords = OH_UdmfData_GetRecords(getData, &getrecordCount);
-    NAPI_ASSERT(env,getrecordCount == 1,"OH_getRecords is fail");
+    NAPI_ASSERT(env, getrecordCount == 1, "OH_getRecords is fail");
 
     unsigned int getCount = 0;
     unsigned char *getEntry;
     int getRes = OH_UdmfRecord_GetGeneralEntry(getRecords[0], typeId, &getEntry, &getCount);
-    NAPI_ASSERT(env,getRes == ERR_OK,"OH_getRecords is fail");
-    NAPI_ASSERT(env,getCount == count,"OH_getRecords is fail");
-    NAPI_ASSERT(env,memcmp(entry, getEntry, getCount) == 0,"OH_getRecords is fail");
+    NAPI_ASSERT(env, getRes == ERR_OK, "OH_getRecords is fail");
+    NAPI_ASSERT(env, getCount == count, "OH_getRecords is fail");
+    NAPI_ASSERT(env, memcmp(entry, getEntry, getCount) == 0, "OH_getRecords is fail");
     napi_create_int32(env, res == ERR_OK, &result);
     OH_Pasteboard_Destroy(pasteboard);
     OH_UdmfRecord_Destroy(record);
@@ -583,42 +550,62 @@ static napi_value OH_Pasteboard_GetData0300(napi_env env, napi_callback_info inf
     return result;
 }
 
-
-
-//======================================================================================================
-//======================================================================================================
-//======================================================================================================
-
-
 EXTERN_C_START
-static napi_value Init(napi_env env, napi_value exports) {
+static napi_value Init(napi_env env, napi_value exports)
+{
     napi_property_descriptor desc[] = {
-        {"OH_PasteboardSubscriber_Create0100", nullptr, OH_PasteboardSubscriber_Create0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_PasteboardObserver_SetData0100", nullptr, OH_PasteboardObserver_SetData0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_PasteboardObserver_SetData0200", nullptr, OH_PasteboardObserver_SetData0200, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_PasteboardObserver_SetData0300", nullptr, OH_PasteboardObserver_SetData0300, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_PasteboardObserver_SetData0400", nullptr, OH_PasteboardObserver_SetData0400, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Create0100", nullptr, OH_Pasteboard_Create0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Subscribe0100", nullptr, OH_Pasteboard_Subscribe0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Subscribe0200", nullptr, OH_Pasteboard_Subscribe0200, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Subscribe0300", nullptr, OH_Pasteboard_Subscribe0300, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Subscribe0400", nullptr, OH_Pasteboard_Subscribe0400, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Subscribe0500", nullptr, OH_Pasteboard_Subscribe0500, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Subscribe0600", nullptr, OH_Pasteboard_Subscribe0600, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Unsubcribe0100", nullptr, OH_Pasteboard_Unsubcribe0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Unsubcribe0200", nullptr, OH_Pasteboard_Unsubcribe0200, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Unsubcribe0300", nullptr, OH_Pasteboard_Unsubcribe0300, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Unsubcribe0400", nullptr, OH_Pasteboard_Unsubcribe0400, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_Unsubcribe0500", nullptr, OH_Pasteboard_Unsubcribe0500, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_IsRemoteData0100", nullptr, OH_Pasteboard_IsRemoteData0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_GetDataSrouce0100", nullptr, OH_Pasteboard_GetDataSrouce0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_HasType0100", nullptr, OH_Pasteboard_HasType0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_HasData0100", nullptr, OH_Pasteboard_HasData0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_ClearData0100", nullptr, OH_Pasteboard_ClearData0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_SetData0100", nullptr, OH_Pasteboard_SetData0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_GetData0100", nullptr, OH_Pasteboard_GetData0100, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_GetData0200", nullptr, OH_Pasteboard_GetData0200, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"OH_Pasteboard_GetData0300", nullptr, OH_Pasteboard_GetData0300, nullptr, nullptr, nullptr, napi_default, nullptr}
+        {"OH_PasteboardSubscriber_Create0100", nullptr, OH_PasteboardSubscriber_Create0100,
+         nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"OH_PasteboardObserver_SetData0100", nullptr, OH_PasteboardObserver_SetData0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_PasteboardObserver_SetData0200", nullptr, OH_PasteboardObserver_SetData0200, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_PasteboardObserver_SetData0300", nullptr, OH_PasteboardObserver_SetData0300, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_PasteboardObserver_SetData0400", nullptr, OH_PasteboardObserver_SetData0400, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Create0100", nullptr, OH_Pasteboard_Create0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Subscribe0100", nullptr, OH_Pasteboard_Subscribe0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Subscribe0200", nullptr, OH_Pasteboard_Subscribe0200, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Subscribe0300", nullptr, OH_Pasteboard_Subscribe0300, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Subscribe0400", nullptr, OH_Pasteboard_Subscribe0400, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Subscribe0500", nullptr, OH_Pasteboard_Subscribe0500, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Subscribe0600", nullptr, OH_Pasteboard_Subscribe0600, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Unsubcribe0100", nullptr, OH_Pasteboard_Unsubcribe0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Unsubcribe0200", nullptr, OH_Pasteboard_Unsubcribe0200, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Unsubcribe0300", nullptr, OH_Pasteboard_Unsubcribe0300, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Unsubcribe0400", nullptr, OH_Pasteboard_Unsubcribe0400, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_Unsubcribe0500", nullptr, OH_Pasteboard_Unsubcribe0500, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_IsRemoteData0100", nullptr, OH_Pasteboard_IsRemoteData0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_GetDataSrouce0100", nullptr, OH_Pasteboard_GetDataSrouce0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_HasType0100", nullptr, OH_Pasteboard_HasType0100, nullptr,
+         nullptr,nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_HasData0100", nullptr, OH_Pasteboard_HasData0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_ClearData0100", nullptr, OH_Pasteboard_ClearData0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_SetData0100", nullptr, OH_Pasteboard_SetData0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_GetData0100", nullptr, OH_Pasteboard_GetData0100, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_GetData0200", nullptr, OH_Pasteboard_GetData0200, nullptr,
+         nullptr, nullptr, napi_default, nullptr},
+        {"OH_Pasteboard_GetData0300", nullptr, OH_Pasteboard_GetData0300, nullptr,
+         nullptr, nullptr, napi_default, nullptr}
     };
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
